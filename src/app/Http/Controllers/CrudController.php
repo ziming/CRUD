0fedb6f6a7b3c26265768b1acb70db44eef47d16
Owen Melbourne <owen@selesti.com>
2016-11-18 12:19:22 +0000

    Exclude _method from massAssignment

--- src/app/Http/Controllers/CrudController.php
+++ src/app/Http/Controllers/CrudController.php
@@ -115,7 +115,7 @@ class CrudController extends BaseController
         }
 
         // insert item in the db
-        $item = $this->crud->create($request->except(['redirect_after_save', '_token']));
+        $item = $this->crud->create($request->except(['redirect_after_save', '_token', '_method']));
 
         // show a success message
         \Alert::success(trans('backpack::crud.insert_success'))->flash();
@@ -175,7 +175,7 @@ class CrudController extends BaseController
 
         // update the row in the db
         $this->crud->update($request->get($this->crud->model->getKeyName()),
-                            $request->except('redirect_after_save', '_token'));
+                            $request->except('redirect_after_save', '_token', '_method'));
 
         // show a success message
         \Alert::success(trans('backpack::crud.update_success'))->flash();

fc81469af2d8c58918296bb5b24b30c361a04760
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-12-22 12:58:51 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/ModelTraits/Translatable/SpatieTranslatableAdaptor.php
+++ src/ModelTraits/Translatable/SpatieTranslatableAdaptor.php
@@ -4,31 +4,28 @@ namespace Backpack\CRUD\ModelTraits\Translatable;
 
 trait SpatieTranslatableAdaptor
 {
-	/**
-	 * Overwrite Eloquent's create method.
-	 *
-	 * @param  array  $attributes [description]
-	 * @return [type]             [description]
-	 */
+    /**
+     * Overwrite Eloquent's create method.
+     *
+     * @param  array  $attributes [description]
+     * @return [type]             [description]
+     */
     public static function create(array $attributes = [])
     {
         $locale = $attributes['locale'];
         $attributes = array_except($attributes, ['locale']);
 
-    	$model = new static();
-    	$translatable_attributes = $model->translatable;
+        $model = new static();
+        $translatable_attributes = $model->translatable;
 
-    	// do the actual saving
-    	foreach ($attributes as $attribute => $value) {
-    		if (in_array($attribute, $translatable_attributes))
-    		{ // the attribute is translatable
-    			$model->setTranslation($attribute, $locale, $value);
-    		}
-    		else
-    		{ // the attribute is NOT translatable
-    			$model->{$attribute} = $value;
-    		}
-    	}
+        // do the actual saving
+        foreach ($attributes as $attribute => $value) {
+            if (in_array($attribute, $translatable_attributes)) { // the attribute is translatable
+                $model->setTranslation($attribute, $locale, $value);
+            } else { // the attribute is NOT translatable
+                $model->{$attribute} = $value;
+            }
+        }
         $model->save();
 
         return $model;
@@ -41,6 +38,6 @@ trait SpatieTranslatableAdaptor
      */
     public function translationEnabledForModel()
     {
-    	return property_exists($this, 'translatable');
+        return property_exists($this, 'translatable');
     }
 }

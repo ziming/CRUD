ed1434cfc21007b1e2f32e3173f454f3a6f3a266
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-02-12 10:35:43 +0200

    fixed saveAction buttons in tabbed forms

--- src/resources/views/form_content.blade.php
+++ src/resources/views/form_content.blade.php
@@ -90,43 +90,54 @@
     @stack('crud_fields_scripts')
 
     <script>
-    jQuery('document').ready(function($){
+        jQuery('document').ready(function($){
 
-        // Ctrl+S and Cmd+S trigger Save button click
-        $(document).keydown(function(e) {
-            if ((e.which == '115' || e.which == '83' ) && (e.ctrlKey || e.metaKey))
-            {
-                e.preventDefault();
-                // alert("Ctrl-s pressed");
-                $("button[type=submit]").trigger('click');
-                return false;
+          //Save button dropdown toggles
+          var saveActions = $('#saveActions'),
+          crudForm        = saveActions.parents('form'),
+          saveActionField = $('[name="save_action"]');
+
+          saveActions.on('click', '.dropdown-menu a', function(){
+              var saveAction = $(this).data('value');
+              saveActionField.val( saveAction );
+              crudForm.submit();
+          });
+
+            // Ctrl+S and Cmd+S trigger Save button click
+            $(document).keydown(function(e) {
+                if ((e.which == '115' || e.which == '83' ) && (e.ctrlKey || e.metaKey))
+                {
+                    e.preventDefault();
+                    // alert("Ctrl-s pressed");
+                    $("button[type=submit]").trigger('click');
+                    return false;
+                }
+                return true;
+            });
+
+          @if( $crud->autoFocusOnFirstField )
+            //Focus on first field
+            @php
+              $focusField = array_first($fields, function($field){
+                  return isset($field['auto_focus']) && $field['auto_focus'] == true;
+              })
+            @endphp
+
+            @if($focusField)
+              window.focusField = $('[name="{{$focusField['name']}}"]').eq(0),
+            @else
+              var focusField = $('form').find('input, textarea, select').not('[type="hidden"]').eq(0),
+            @endif
+
+            fieldOffset = focusField.offset().top,
+            scrollTolerance = $(window).height() / 2;
+
+            focusField.trigger('focus');
+
+            if( fieldOffset > scrollTolerance ){
+                $('html, body').animate({scrollTop: (fieldOffset - 30)});
             }
-            return true;
+          @endif
         });
-
-        @if( $crud->autoFocusOnFirstField )
-        //Focus on first field
-        @php
-        $focusField = array_first($fields, function($field){
-            return isset($field['auto_focus']) && $field['auto_focus'] == true;
-        })
-        @endphp
-
-        @if($focusField)
-        window.focusField = $('[name="{{$focusField['name']}}"]').eq(0),
-        @else
-        var focusField = $('form').find('input, textarea, select').not('[type="hidden"]').eq(0),
-        @endif
-
-        fieldOffset = focusField.offset().top,
-        scrollTolerance = $(window).height() / 2;
-
-        focusField.trigger('focus');
-
-        if( fieldOffset > scrollTolerance ){
-            $('html, body').animate({scrollTop: (fieldOffset - 30)});
-        }
-        @endif
-    });
     </script>
 @endsection

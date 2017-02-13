73317232afa76350e8783de00cc1d8332edc3fe3
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-02-13 08:35:22 +0200

    change crudpanel class injection; fix simplemde full screen; add select2 ajax and multiple

--- src/resources/views/fields/simplemde.blade.php
+++ src/resources/views/fields/simplemde.blade.php
@@ -22,6 +22,11 @@
     {{-- FIELD CSS - will be loaded in the after_styles section --}}
     @push('crud_fields_styles')
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+        <style type="text/css">
+        .CodeMirror-fullscreen, .editor-toolbar.fullscreen {
+            z-index: 9999 !important;
+        }
+        </style>
     @endpush
 
     {{-- FIELD JS - will be loaded in the after_scripts section --}}

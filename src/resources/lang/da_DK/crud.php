dc5c9f71507f46da305b07bff4899865224583a4
Frederik Rabøl <frederik-rm@hotmail.com>
2017-08-04 20:03:21 +0200

    added author information

--- src/resources/lang/da_DK/crud.php
+++ src/resources/lang/da_DK/crud.php
@@ -8,8 +8,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | The following language lines are used by the CRUD interface.
-    | You are free to change them to anything
-    | you want to customize your views to better match your application.
+    | Author: Frederik Rabøl Madsen <frederik-rm@hotmail.com>
     |
     */
 

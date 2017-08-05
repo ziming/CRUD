1db78e426d2509d29338385b5899337b56c673ce
Oliver Ziegler <oliver.ziegler@zoutapps.de>
2017-08-05 15:35:45 +0200

    add CRUDColumns facade

---
+++ src/Facades/CRUDColumns.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Backpack\CRUD\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class CRUDColumns extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'CRUDColumns';
+    }
+
+}

1804e2835057f0f01f06960f7f31c489721e2887
Catalin Tudorache <tumf87@gmail.com>
2017-08-21 13:51:42 +0300

    Moved else to previous line.

--- src/resources/views/columns/select.blade.php
+++ src/resources/views/columns/select.blade.php
@@ -1,11 +1,10 @@
 {{-- single relationships (1-1, 1-n) --}}
 <td>
 	<?php
-		$relationArray = explode(".", $column['entity']);
+		$relationArray = explode('.', $column['entity']);
 		if(count($relationArray) == 1 && $entry->{$column['entity']}) {
             echo $entry->{$column['entity']}->{$column['attribute']};
-		}
-		else {
+		} else {
             $resultsArray = $crud->getAttributeFromNestedRelations($entry, $column['entity'], $column['attribute']);
             if (count($resultsArray)) {
                 echo implode(', ', $resultsArray);

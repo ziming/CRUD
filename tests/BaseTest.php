eba4ffbe8e594ac500c80200353b7714e1705e0c
Catalin Tudorache <tumf87@gmail.com>
2017-08-31 09:15:39 +0300

    Fixed class name after merge.

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -4,7 +4,7 @@ namespace Backpack\CRUD\Tests;
 
 use Orchestra\Testbench\TestCase;
 
-abstract class TestCase extends TestCase
+abstract class BaseTest extends TestCase
 {
     protected function setUp()
     {

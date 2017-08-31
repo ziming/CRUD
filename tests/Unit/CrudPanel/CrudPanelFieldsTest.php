0d0a76f7aa5b8330d719f19e3f4b7296fad19c20
Catalin Tudorache <tumf87@gmail.com>
2017-08-31 09:45:51 +0300

    Implemented before field tests

--- tests/Unit/CrudPanel/CrudPanelFieldsTest.php
+++ tests/Unit/CrudPanel/CrudPanelFieldsTest.php
@@ -27,8 +27,20 @@ class CrudPanelFieldsTest extends BaseCrudPanelTest
         [
             'name' => 'field2',
             'label' => 'Field2',
+        ],
+    ];
+
+    private $expectedTwoTextFieldsArray = [
+        'field1' => [
+            'name' => 'field1',
+            'label' => 'Field1',
             'type' => 'text',
         ],
+        'field2' => [
+            'name' => 'field2',
+            'label' => 'Field2',
+            'type' => 'text',
+        ]
     ];
 
     private $threeTextFieldsArray = [
@@ -40,14 +52,12 @@ class CrudPanelFieldsTest extends BaseCrudPanelTest
         [
             'name' => 'field2',
             'label' => 'Field2',
-            'type' => 'text',
         ],
         [
             'name' => 'field3',
-            'label' => 'Field3',
-            'type' => 'text',
         ],
     ];
+
     private $expectedThreeTextFieldsArray = [
         'field1' => [
             'name' => 'field1',
@@ -119,22 +129,93 @@ class CrudPanelFieldsTest extends BaseCrudPanelTest
         $this->assertEmpty($this->crudPanel->create_fields);
     }
 
+    public function testBeforeField()
+    {
+        $this->markTestIncomplete("Not correctly implemented");
+
+        $this->crudPanel->addFields($this->threeTextFieldsArray);
+
+        // TODO: fix the before field method not preserving field keys
+        $this->crudPanel->beforeField('field1');
+
+        $createKeys = array_keys($this->crudPanel->create_fields);
+        $this->assertEquals($this->expectedThreeTextFieldsArray['field3'], $this->crudPanel->create_fields[$createKeys[0]]);
+        $this->assertEquals(['field3', 'field1', 'field2'], $createKeys);
+
+        $updateKeys = array_keys($this->crudPanel->update_fields);
+        $this->assertEquals($this->expectedThreeTextFieldsArray['field3'], $this->crudPanel->update_fields[$updateKeys[0]]);
+        $this->assertEquals(['field3', 'field1', 'field2'], $updateKeys);
+    }
+
+    public function testBeforeFieldCreateForm()
+    {
+        $this->markTestIncomplete("Not correctly implemented");
+
+        $this->crudPanel->addFields($this->threeTextFieldsArray, 'create');
+
+        // TODO: fix the before field method not preserving field keys
+        $this->crudPanel->beforeField('field1');
+
+        $createKeys = array_keys($this->crudPanel->create_fields);
+        $this->assertEquals($this->expectedThreeTextFieldsArray['field3'], $this->crudPanel->create_fields[$createKeys[0]]);
+        $this->assertEquals(['field3', 'field1', 'field2'], $createKeys);
+
+        $this->assertEmpty($this->crudPanel->update_fields);
+    }
+
+    public function testBeforeFieldUpdateForm()
+    {
+        $this->markTestIncomplete("Not correctly implemented");
+
+        $this->crudPanel->addFields($this->threeTextFieldsArray, 'update');
+
+        // TODO: fix the before field method not preserving field keys
+        $this->crudPanel->beforeField('field1');
+
+        $updateKeys = array_keys($this->crudPanel->update_fields);
+        $this->assertEquals($this->expectedThreeTextFieldsArray['field3'], $this->crudPanel->update_fields[$updateKeys[0]]);
+        $this->assertEquals(['field3', 'field1', 'field2'], $updateKeys);
+
+        $this->assertEmpty($this->crudPanel->create_fields);
+    }
+
+    public function testBeforeFieldForDifferentFieldsInCreateAndUpdate()
+    {
+        $this->markTestIncomplete("Not correctly implemented");
+
+        $this->crudPanel->addFields($this->threeTextFieldsArray, 'create');
+        $this->crudPanel->addFields($this->twoTextFieldsArray, 'update');
+
+        // TODO: fix the before field method not preserving field keys
+        // TODO: in the case of the create form, this will move the 'field3' field before the 'field1' field, but for
+        //       the update form, it will move the 'field2' field before the 'field1' field. should it work like this?
+        $this->crudPanel->beforeField('field1');
+
+        $createKeys = array_keys($this->crudPanel->create_fields);
+        $this->assertEquals($this->expectedThreeTextFieldsArray['field3'], $this->crudPanel->create_fields[$createKeys[0]]);
+        $this->assertEquals(['field3', 'field1', 'field2'], $createKeys);
+
+        $updateKeys = array_keys($this->crudPanel->update_fields);
+        $this->assertEquals($this->expectedTwoTextFieldsArray['field2'], $this->crudPanel->update_fields[$updateKeys[0]]);
+        $this->assertEquals(['field2', 'field1'], $updateKeys);
+    }
+
+    public function testBeforeUnknownField()
+    {
+        $this->crudPanel->addFields($this->threeTextFieldsArray);
+
+        $this->crudPanel->beforeField('field4');
+
+        $this->assertEquals(array_keys($this->expectedThreeTextFieldsArray), array_keys($this->crudPanel->create_fields));
+        $this->assertEquals(array_keys($this->expectedThreeTextFieldsArray), array_keys($this->crudPanel->update_fields));
+    }
+
     public function testAfterField()
     {
         $this->markTestIncomplete();
     }
 
     public function testAfterUnknownField()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testBeforeField()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testBeforeUnknownField()
     {
         $this->markTestIncomplete();
     }
@@ -172,5 +253,7 @@ class CrudPanelFieldsTest extends BaseCrudPanelTest
     public function testDecodeJsonCastedAttributes()
     {
         $this->markTestIncomplete();
+
+        // TODO: the decode JSON method should not be in fields trait and should not be exposed in the public API.
     }
 }

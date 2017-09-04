c72708cfa630c64dcc6faaa5d26cf0bf23e30d49
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-09-04 13:10:12 +0300

    fixes #555 - hidden field types should have no height

--- src/resources/views/inc/field_wrapper_attributes.blade.php
+++ src/resources/views/inc/field_wrapper_attributes.blade.php
@@ -8,6 +8,8 @@
     @if (!isset($field['wrapperAttributes']['class']))
 		class="form-group col-md-12"
     @endif
+@elseif ($field['type'] == 'hidden')
+	class="hidden"
 @else
 	class="form-group col-md-12"
 @endif

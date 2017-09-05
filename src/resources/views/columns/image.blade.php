3e83045da01ca029448b36aa12c1cb6758cd74c8
Hugo Aguirre <bul.ikana@gmail.com>
2017-09-05 17:18:17 -0500

    Add improvements discussed on the PR

--- src/resources/views/columns/image.blade.php
+++ src/resources/views/columns/image.blade.php
@@ -1,13 +1,13 @@
 <td>
-  @if( !empty($entry->{$column['src']}) )
+  @if( !empty($entry->{$column['name']}) )
     <a
-      href="{{ $entry->{$column['src']} }}"
+      href="{{ asset($entry->{$column['name']}) }}"
       target="_blank"
     >
       <img
-        src="{{ $entry->{$column['src']} }}"
+        src="{{ asset($entry->{$column['name']}) }}"
         style="
-          height: {{ isset($column['height']) ? $column['height'] : "25px" }};
+          max-height: {{ isset($column['height']) ? $column['height'] : "25px" }};
           width: {{ isset($column['width']) ? $column['width'] : "auto" }};
           border-radius: 3px;"
       />

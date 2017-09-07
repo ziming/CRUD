fbaa51d17563ccbde10a7e6efa612a9a760f7060
Catalin Tudorache <tumf87@gmail.com>
2017-09-07 16:33:50 +0300

    Added Delete trait tests.

---
+++ tests/Unit/CrudPanel/CrudPanelDeleteTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Backpack\CRUD\Tests\Unit\CrudPanel;
+
+use Backpack\CRUD\Tests\Unit\Models\Article;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
+
+class CrudPanelDeleteTest extends BaseDBCrudPanelTest
+{
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not correctly implemented');
+
+        $this->crudPanel->setModel(Article::class);
+        $article = Article::find(1);
+
+        $wasDeleted = $this->crudPanel->delete($article->id);
+
+        // TODO: the delete method should not convert the returned result to a string
+        $deletedArticle = Article::find(1);
+        $this->assertTrue($wasDeleted);
+        $this->assertNull($deletedArticle);
+    }
+
+    public function testDeleteUnknown()
+    {
+        $this->markTestIncomplete('Not correctly implemented');
+
+        $this->setExpectedException(ModelNotFoundException::class);
+
+        $this->crudPanel->setModel(Article::class);
+        $unknownId = DB::getPdo()->lastInsertId() + 1;
+
+        // TODO: if the delete method is called with an unknown id, it should return a model not found exception. change
+        //       the find call inside the delete method to a findOrFail to avoid the error.
+        $this->crudPanel->delete($unknownId);
+    }
+}

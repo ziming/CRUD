1046a566f02fb1748dbd0b8ace16711265f99653
Emanuele <EmanueleCoppola@users.noreply.github.com>
2017-10-19 23:22:05 +0200

    Update crud.php
    This is better, sorry for too many commits

--- src/resources/lang/it/crud.php
+++ src/resources/lang/it/crud.php
@@ -34,13 +34,13 @@ return [
     'revisions'            => 'Revisioni',
     'no_revisions'         => 'Nessuna revisione trovata',
     'created_this'         => 'ha creato questo',
-    'changed_the'          => 'cambiato il',
+    'changed_the'          => 'ha cambiato il',
     'restore_this_value'   => 'ripristinare questo valore',
     'from'                 => 'da',
     'to'                   => 'a',
     'undo'                 => 'Annulla',
-    'revision_restored'    => 'Revisione successo restaurato',
-    'guest_user'           => 'Utente Ospite',
+    'revision_restored'    => 'Revisione ripristinata con successo',
+    'guest_user'           => 'Un Ospite',
 
     // Translatable models
     'edit_translations' => 'MODIFICA TRADUZIONI',

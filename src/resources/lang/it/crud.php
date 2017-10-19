19086a7dff670b4345d5aba7b149ec0e9d57ea7a
Emanuele <EmanueleCoppola@users.noreply.github.com>
2017-10-19 22:56:01 +0200

    Update crud.php

--- src/resources/lang/it/crud.php
+++ src/resources/lang/it/crud.php
@@ -36,9 +36,9 @@ return [
     'created_this'         => 'ha creato questo',
     'changed_the'          => 'cambiato il',
     'restore_this_value'   => 'ripristinare questo valore',
-    'from'                 => 'de parte di',
+    'from'                 => 'da parte di',
     'to'                   => 'a',
-    'undo'                 => 'Disfare',
+    'undo'                 => 'Annulla',
     'revision_restored'    => 'Revisione successo restaurato',
     'guest_user'           => 'Utente Ospite',
 

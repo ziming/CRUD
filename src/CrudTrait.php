a46ce9b64275e360b07457d56f395ce14f7671e0
Oliver Ziegler <oliver.ziegler@zoutapps.de>
2017-11-16 19:00:51 +0100

    add/improve comments

--- src/CrudTrait.php
+++ src/CrudTrait.php
@@ -108,6 +108,12 @@ trait CrudTrait
         return $this;
     }
 
+    /**
+     * Determine if this fake column should get json_encoded or not
+     *
+     * @param $column string fake column name
+     * @return bool
+     */
     public function shouldEncodeFake($column)
     {
         return ! in_array($column, array_keys($this->casts));
--- src/PanelTraits/FakeFields.php
+++ src/PanelTraits/FakeFields.php
@@ -35,7 +35,7 @@ trait FakeFields
             }
         }
 
-        // json_encode all fake_value columns in the database, so they can be properly stored and interpreted
+        // json_encode all fake_value columns if applicable in the database, so they can be properly stored and interpreted
         foreach ($compactedFakeFields as $value) {
             if (! (property_exists($this->model, 'translatable') && in_array($value, $this->model->getTranslatableAttributes(), true)) && $this->model->shouldEncodeFake($value)) {
                 $requestInput[$value] = json_encode($requestInput[$value]);

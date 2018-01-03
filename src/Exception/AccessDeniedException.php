e82967ab17ee3df1e48148d94b2ee223f82ff364
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-01-03 08:40:35 +0200

    access denied exception throws 403 error

--- src/Exception/AccessDeniedException.php
+++ src/Exception/AccessDeniedException.php
@@ -4,4 +4,14 @@ namespace Backpack\CRUD\Exception;
 
 class AccessDeniedException extends \UnexpectedValueException
 {
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        return response(view('errors.403'), 403);
+    }
 }

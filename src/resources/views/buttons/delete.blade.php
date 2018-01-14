59517c20caadf1b2bd1c60bae8d7e1b65850e20c
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-01-14 16:31:54 +0200

    list view on mobile and desktop - delete button works on both

--- src/resources/views/buttons/delete.blade.php
+++ src/resources/views/buttons/delete.blade.php
@@ -1,52 +1,55 @@
 @if ($crud->hasAccess('delete'))
-	<a href="{{ url($crud->route.'/'.$entry->getKey()) }}" class="btn btn-xs btn-default" data-button-type="delete"><i class="fa fa-trash"></i> {{ trans('backpack::crud.delete') }}</a>
+	<a href="javascript:void(0)" onclick="deleteEntry(this)" data-route="{{ url($crud->route.'/'.$entry->getKey()) }}" class="btn btn-xs btn-default" data-button-type="delete"><i class="fa fa-trash"></i> {{ trans('backpack::crud.delete') }}</a>
 @endif
 
 <script>
-	if (typeof registerDeleteButtonAction != 'function') {
-	  function registerDeleteButtonAction() {
-        $("[data-button-type=delete]").unbind('click');
-        // CRUD Delete
-        // ask for confirmation before deleting an item
-        $("[data-button-type=delete]").click(function(e) {
-          e.preventDefault();
-          var delete_button = $(this);
-          var delete_url = $(this).attr('href');
+	if (typeof deleteEntry != 'function') {
+	  $("[data-button-type=delete]").unbind('click');
 
-          if (confirm("{{ trans('backpack::crud.delete_confirm') }}") == true) {
-              $.ajax({
-                  url: delete_url,
-                  type: 'DELETE',
-                  success: function(result) {
-                      // Show an alert with the result
-                      new PNotify({
-                          title: "{{ trans('backpack::crud.delete_confirmation_title') }}",
-                          text: "{{ trans('backpack::crud.delete_confirmation_message') }}",
-                          type: "success"
-                      });
-                      // delete the row from the table
-                      delete_button.parentsUntil('tr').parent().remove();
-                  },
-                  error: function(result) {
-                      // Show an alert with the result
-                      new PNotify({
-                          title: "{{ trans('backpack::crud.delete_confirmation_not_title') }}",
-                          text: "{{ trans('backpack::crud.delete_confirmation_not_message') }}",
-                          type: "warning"
-                      });
-                  }
-              });
-          } else {
-              new PNotify({
-                  title: "{{ trans('backpack::crud.delete_confirmation_not_deleted_title') }}",
-                  text: "{{ trans('backpack::crud.delete_confirmation_not_deleted_message') }}",
-                  type: "info"
-              });
-          }
-        });
+	  function deleteEntry(button) {
+	      // ask for confirmation before deleting an item
+	      // e.preventDefault();
+	      var button = $(button);
+	      var route = button.attr('data-route');
+	      var row = $("#crudTable a[data-route='"+route+"']").parentsUntil('tr').parent();
+
+	      if (confirm("{{ trans('backpack::crud.delete_confirm') }}") == true) {
+	          $.ajax({
+	              url: route,
+	              type: 'DELETE',
+	              success: function(result) {
+	                  // Show an alert with the result
+	                  new PNotify({
+	                      title: "{{ trans('backpack::crud.delete_confirmation_title') }}",
+	                      text: "{{ trans('backpack::crud.delete_confirmation_message') }}",
+	                      type: "success"
+	                  });
+
+	                  // close the modal, if any
+	                  $('.modal').modal('hide');
+
+	                  // delete the row from the table
+	                  row.remove();
+	              },
+	              error: function(result) {
+	                  // Show an alert with the result
+	                  new PNotify({
+	                      title: "{{ trans('backpack::crud.delete_confirmation_not_title') }}",
+	                      text: "{{ trans('backpack::crud.delete_confirmation_not_message') }}",
+	                      type: "warning"
+	                  });
+	              }
+	          });
+	      } else {
+	          new PNotify({
+	              title: "{{ trans('backpack::crud.delete_confirmation_not_deleted_title') }}",
+	              text: "{{ trans('backpack::crud.delete_confirmation_not_deleted_message') }}",
+	              type: "info"
+	          });
+	      }
       }
 	}
 
 	// make it so that the function above is run after each DataTable draw event
-	crud.addFunctionToDataTablesDrawEventQueue('registerDeleteButtonAction');
+	// crud.addFunctionToDataTablesDrawEventQueue('deleteEntry');
 </script>

19b3d81b78cf242d904285d2a8a7eaa4eb5e71d6
Oliver Ziegler <oliver.ziegler@webfactor.de>
2018-02-08 12:48:10 +0100

    add missing closing tag

--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -52,7 +52,7 @@
 		        @endforeach
 				@if ($crud->buttons->where('stack', 'line')->count())
 					<tr>
-						<td><strong>{{ trans('backpack::crud.actions') }}</td>
+						<td><strong>{{ trans('backpack::crud.actions') }}</strong></td>
 						<td>
 							@include('crud::inc.button_stack', ['stack' => 'line'])
 						</td>

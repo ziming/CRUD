b1d95b2285f5bc165f33859a64c8c676b3fddee0
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-02-14 09:52:30 +0200

    Apply fixes from StyleCI (#1236)
    [ci skip] [skip ci]

--- src/app/Http/Controllers/CrudController.php
+++ src/app/Http/Controllers/CrudController.php
@@ -4,19 +4,19 @@ namespace Backpack\CRUD\app\Http\Controllers;
 
 use Backpack\CRUD\CrudPanel;
 use Illuminate\Http\Request;
+use Illuminate\Http\Request as StoreRequest;
 use Illuminate\Support\Facades\Form as Form;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request as UpdateRequest;
+// CRUD Traits for non-core features
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-// CRUD Traits for non-core features
 use Backpack\CRUD\app\Http\Controllers\CrudFeatures\Reorder;
 use Backpack\CRUD\app\Http\Controllers\CrudFeatures\AjaxTable;
 use Backpack\CRUD\app\Http\Controllers\CrudFeatures\Revisions;
+// Default request classes
 use Backpack\CRUD\app\Http\Controllers\CrudFeatures\SaveActions;
 use Backpack\CRUD\app\Http\Controllers\CrudFeatures\ShowDetailsRow;
-// Default request classes
-use Illuminate\Http\Request as StoreRequest;
-use Illuminate\Http\Request as UpdateRequest;
 
 class CrudController extends BaseController
 {

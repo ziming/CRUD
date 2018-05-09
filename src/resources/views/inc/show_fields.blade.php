f3259444f088430d56ebea86705eba335605e04d
Thomas Swonke <thomas.swonke@webfactor.de>
2018-05-09 16:01:59 +0200

    [EH] add possibility to use custom fields

--- src/resources/views/inc/show_fields.blade.php
+++ src/resources/views/inc/show_fields.blade.php
@@ -1,9 +1,14 @@
 {{-- Show the inputs --}}
 @foreach ($fields as $field)
-    <!-- load the view from the application if it exists, otherwise load the one in the package -->
-    @if(view()->exists('vendor.backpack.crud.fields.'.$field['type']))
-        @include('vendor.backpack.crud.fields.'.$field['type'], array('field' => $field))
+    <!-- load the view from the view attribute if set -->
+    @if(isset($field['view']))
+        @include($field['view'].'.'.$field['type'], ['field' => $field])
     @else
-        @include('crud::fields.'.$field['type'], array('field' => $field))
+        <!-- load the view from the application if it exists, otherwise load the one in the package -->
+        @if(view()->exists('vendor.backpack.crud.fields.'.$field['type']))
+            @include('vendor.backpack.crud.fields.'.$field['type'], ['field' => $field])
+        @else
+            @include('crud::fields.'.$field['type'], ['field' => $field])
+        @endif
     @endif
 @endforeach

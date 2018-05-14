429951a7a4401ed9a1dc1668817f818d7aac3497
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-05-14 18:34:18 +0000

    polished phone field type

--- src/resources/views/fields/phone.blade.php
+++ src/resources/views/fields/phone.blade.php
@@ -3,16 +3,13 @@
     <label>{!! $field['label'] !!}</label>
     @include('crud::inc.field_translatable_icon')
 
-    @if(isset($field['prefix']) || isset($field['suffix'])) <div class="input-group"> @endif
-        @if(isset($field['prefix'])) <div class="input-group-addon">{!! $field['prefix'] !!}</div> @endif
-        <input
-            type="tel"
-            name="{{ $field['name'] }}"
-            value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
-            @include('crud::inc.field_attributes')
-        >
-        @if(isset($field['suffix'])) <div class="input-group-addon">{!! $field['suffix'] !!}</div> @endif
-    @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
+    <input
+        type="tel"
+        id="{{ $field['name'] }}_picker"
+        value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
+        @include('crud::inc.field_attributes')
+    >
+    <input type="hidden" name="{{ $field['name'] }}" value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}">
 
     {{-- HINT --}}
     @if (isset($field['hint']))
@@ -27,7 +24,7 @@
 
     {{-- FIELD CSS - will be loaded in the after_styles section --}}
     @push('crud_fields_styles')
-        <link rel="stylesheet" href="{{ asset('vendor/backpack/telephone/src/css/intlTelInput.css') }}">
+        <link rel="stylesheet" href="{{ asset('vendor/backpack/intl-tel-input/css/intlTelInput.css') }}">
         <style>
           .intl-tel-input {
             display: flex;
@@ -38,8 +35,7 @@
 
     {{-- FIELD JS - will be loaded in the after_scripts section --}}
     @push('crud_fields_scripts')
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <script src="{{ asset('vendor/backpack/telephone/src/js/intlTelInput.js') }}"></script>
+      <script src="{{ asset('vendor/backpack/intl-tel-input/js/intlTelInput.min.js') }}"></script>
     @endpush
 
 @endif
@@ -48,7 +44,31 @@
 <script type="text/javascript">
     jQuery('document').ready(function($){
       // https://github.com/jackocnr/intl-tel-input
-      $('[name="{{ $field['name'] }}"]').intlTelInput();
+      var input = $('[id="{{ $field['name'] }}_picker"]');
+      var output = $('[name="{{ $field['name'] }}"]');
+
+      input.intlTelInput({
+          initialCountry: "auto",
+          separateDialCode: true,
+          nationalMode: true,
+          autoHideDialCode: false,
+          hiddenInput: "{{ $field['name'] }}",
+          geoIpLookup: function(callback) {
+            $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+              var countryCode = (resp && resp.country) ? resp.country : "";
+              callback(countryCode);
+            });
+          },
+          utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.15/js/utils.js"
+        });
+
+      input.on("keyup change", function() {
+          var intlNumber = input.intlTelInput("getNumber");
+
+          if (intlNumber) {
+            output.val(intlNumber);
+          }
+        });
     });
 </script>
 @endpush

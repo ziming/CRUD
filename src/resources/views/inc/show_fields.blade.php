3a3ab892dc0c82bd3f1f9129e1a27a41f4a2e136
Thomas Swonke <thomas.swonke@webfactor.de>
2018-05-16 10:36:22 +0200

    rename view to view_namespace

--- src/resources/views/inc/show_fields.blade.php
+++ src/resources/views/inc/show_fields.blade.php
@@ -1,8 +1,8 @@
 {{-- Show the inputs --}}
 @foreach ($fields as $field)
     <!-- load the view from the view attribute if set -->
-    @if(isset($field['view']))
-        @include($field['view'].'.'.$field['type'], ['field' => $field])
+    @if(isset($field['view_namespace']))
+        @include($field['view_namespace'].'.'.$field['type'], ['field' => $field])
     @else
         <!-- load the view from the application if it exists, otherwise load the one in the package -->
         @if(view()->exists('vendor.backpack.crud.fields.'.$field['type']))

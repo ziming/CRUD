d6615e5412e0c1f1d084f05bc642d97bb3d47683
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-06-28 12:31:11 +0300

    hide back buttons and unnecessary features from print previews

--- src/resources/views/create.blade.php
+++ src/resources/views/create.blade.php
@@ -19,7 +19,7 @@
 	<div class="col-md-8 col-md-offset-2">
 		<!-- Default box -->
 		@if ($crud->hasAccess('list'))
-			<a href="{{ url($crud->route) }}"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
+			<a href="{{ url($crud->route) }}" class="hidden-print"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
 		@endif
 
 		@include('crud::inc.grouped_errors')

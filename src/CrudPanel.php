a62141b9ee7f60c220d76f366d8357f7f4b6f30d
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-07-04 16:57:28 +0300

    fixes #1493

--- src/CrudPanel.php
+++ src/CrudPanel.php
@@ -6,6 +6,7 @@ use Backpack\CRUD\PanelTraits\Read;
 use Backpack\CRUD\PanelTraits\Tabs;
 use Backpack\CRUD\PanelTraits\Query;
 use Backpack\CRUD\PanelTraits\Views;
+use Backpack\CRUD\PanelTraits\RequiredFields;
 use Backpack\CRUD\PanelTraits\Access;
 use Backpack\CRUD\PanelTraits\Create;
 use Backpack\CRUD\PanelTraits\Delete;
@@ -26,7 +27,7 @@ use Backpack\CRUD\PanelTraits\ViewsAndRestoresRevisions;
 
 class CrudPanel
 {
-    use Create, Read, Search, Update, Delete, Errors, Reorder, Access, Columns, Fields, Query, Buttons, AutoSet, FakeFields, FakeColumns, ViewsAndRestoresRevisions, AutoFocus, Filters, Tabs, Views;
+    use Create, Read, Search, Update, Delete, Errors, Reorder, Access, Columns, Fields, Query, Buttons, AutoSet, FakeFields, FakeColumns, ViewsAndRestoresRevisions, AutoFocus, Filters, Tabs, Views, RequiredFields;
 
     // --------------
     // CRUD variables
---
+++ src/PanelTraits/RequiredFields.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Backpack\CRUD\PanelTraits;
+
+trait RequiredFields
+{
+    /*
+    |--------------------------------------------------------------------------
+    |                             REQUIRED FIELDS
+    |--------------------------------------------------------------------------
+    */
+    public $requiredFields = [];
+
+    public function setRequired($class, $operation)
+    {
+        $formRequest = new $class;
+
+        $rules = $formRequest->rules();
+
+        if (count($rules)) {
+            foreach ($rules as $key => $rule) {
+                if (strpos($rule, 'required') !== false) {
+                    $this->requiredFields[$operation][] = $key;
+                }
+            }
+        }
+    }
+
+    public function isRequired($inputName, $operation)
+    {
+        if (!isset($this->requiredFields[$operation])) {
+            return false;
+        }
+
+        return in_array($inputName, $this->requiredFields[$operation]);
+    }
+}

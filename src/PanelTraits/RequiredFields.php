5f720b06f11a89ee388a2ed0df567c7fb48a6f18
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-07-04 17:08:27 +0300

    added phpdocs

--- src/PanelTraits/RequiredFields.php
+++ src/PanelTraits/RequiredFields.php
@@ -11,6 +11,13 @@ trait RequiredFields
     */
     public $requiredFields = [];
 
+    /**
+     * Parse a FormRequest class, figure out what inputs are required
+     * and store this knowledge in the current object.
+     *
+     * @param [type] $class     Class that extends FormRequest
+     * @param [type] $operation create / update
+     */
     public function setRequired($class, $operation)
     {
         $formRequest = new $class;
@@ -26,6 +33,13 @@ trait RequiredFields
         }
     }
 
+    /**
+     * Check the current object to see if an input is required
+     * for the given operation.
+     * @param  [type]  $inputName Field or input name.
+     * @param  [type]  $operation create / update
+     * @return boolean
+     */
     public function isRequired($inputName, $operation)
     {
         if (!isset($this->requiredFields[$operation])) {

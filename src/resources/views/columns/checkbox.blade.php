51721bc9e126231d7d5f233b1f19b8d1a9ed4cc7
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-08-26 10:38:30 +0300

    bulk delete button - persistent checkbox across pagination and filters

--- src/resources/views/columns/checkbox.blade.php
+++ src/resources/views/columns/checkbox.blade.php
@@ -29,4 +29,25 @@
 		}
 	  }
 	}
+
+	if (typeof markCheckboxAsCheckedIfPreviouslySelected != 'function') {
+	  function markCheckboxAsCheckedIfPreviouslySelected() {
+	  	$('#crudTable input[type=checkbox][data-primary-key-value]').each(function(i, element) {
+			var checked = element.checked;
+			var primaryKeyValue = $(element).attr('data-primary-key-value');
+
+	  		if (typeof crud.checkedItems !== 'undefined' && crud.checkedItems.length > 0)
+	  		{
+	  			var index = crud.checkedItems.indexOf(primaryKeyValue);
+				if (index > -1) {
+					element.checked = true;
+				}
+			}
+	  	});
+	  }
+	}
+
+	// activate checkbox if the page reloaded and the item is remembered as selected
+	// make it so that the function above is run after each DataTable draw event
+	crud.addFunctionToDataTablesDrawEventQueue('markCheckboxAsCheckedIfPreviouslySelected');
 </script>

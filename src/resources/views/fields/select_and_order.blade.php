de8ad7bd997112fd9ffb332919e48856ecd66b41
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-11-12 10:49:49 +0200

    better select_and_order design

--- src/resources/views/fields/select_and_order.blade.php
+++ src/resources/views/fields/select_and_order.blade.php
@@ -7,34 +7,34 @@
     <label>{!! $field['label'] !!}</label>
     @include('crud::inc.field_translatable_icon')
     <div>
-        <ul id="{{ $field['name'] }}_selected" class="{{ $field['name'] }}_connectedSortable select_and_order_selected col-xs-6 pull-left">
+        <ul id="{{ $field['name'] }}_selected" class="{{ $field['name'] }}_connectedSortable select_and_order_selected pull-left">
             @if(old($field["name"]))
                 @if(is_array(old($field["name"])))
                     @foreach (old($field["name"]) as $key)
                         @if(array_key_exists($key,$field['options']))
-                            <li value="{{$key}}">{{ $field['options'][$key] }}</li>
+                            <li value="{{$key}}"><i class="fa fa-arrows"></i> {{ $field['options'][$key] }}</li>
                         @endif
                     @endforeach
                 @endif
             @elseif (is_array($values))
                 @foreach ($values as $key)
                     @if(array_key_exists($key,$field['options']))
-                    <li value="{{$key}}">{{ $field['options'][$key] }}</li>
+                    <li value="{{$key}}"><i class="fa fa-arrows"></i> {{ $field['options'][$key] }}</li>
                     @endif
                 @endforeach
             @endif
         </ul>
-        <ul id="{{ $field['name'] }}_all" class="{{ $field['name'] }}_connectedSortable select_and_order_all col-xs-6 pull-right">
+        <ul id="{{ $field['name'] }}_all" class="{{ $field['name'] }}_connectedSortable select_and_order_all pull-right">
             @if(old($field["name"]))
                 @foreach ($field['options'] as $key => $value)
                     @if(!is_array(old($field["name"])) || !in_array($key, old($field["name"])))
-                        <li value="{{ $key}}">{{ $value }}</li>
+                        <li value="{{ $key}}"><i class="fa fa-arrows"></i> {{ $value }}</li>
                     @endif
                 @endforeach
             @elseif (isset($field['options']))
                 @foreach ($field['options'] as $key => $value)
                     @if(is_array($values) && !in_array($key, $values))
-                        <li value="{{ $key}}">{{ $value }}</li>
+                        <li value="{{ $key}}"><i class="fa fa-arrows"></i> {{ $value }}</li>
                     @endif
                 @endforeach
             @endif
@@ -63,7 +63,7 @@
     <style>
         .select_and_order_all,
         .select_and_order_selected {
-            min-height: 40px;
+            min-height: 120px;
             list-style-type: none;
             max-height: 220px;
             overflow: scroll;
@@ -72,9 +72,8 @@
             border: 1px solid #e6e6e6;
             width: 48%;
         }
-        .select_and_order_all li {
-            background: #fbfbfb;
-            color: grey;
+        .select_and_order_all {
+            border: none;
         }
         .select_and_order_all li,
         .select_and_order_selected li{
@@ -86,6 +85,13 @@
             cursor: grab;
             border-style: dashed;
         }
+        .select_and_order_all li {
+            background: #fbfbfb;
+            color: grey;
+        }
+        .select_and_order_selected li {
+            border-style: solid;
+        }
         .select_and_order_all li.ui-sortable-helper,
         .select_and_order_selected li.ui-sortable-helper {
             color: #3c8dbc;
@@ -94,14 +100,19 @@
         }
         .select_and_order_all .ui-sortable-placeholder,
         .select_and_order_selected .ui-sortable-placeholder {
-            background-color: #3c8dbc;
-            visibility: visible;
+            border: 1px dashed #3c8dbc;
+            visibility: visible!important;
         }
+        .ui-sortable-handle {
+            -ms-touch-action: none;
+            touch-action: none;
+        }
+
     </style>
     @endpush
 
     @push('crud_fields_scripts')
-    <script src="{{ asset('vendor/adminlte/bower_components/jquery-ui/jquery-ui.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/bower_components/jquery-ui/ui/sortable.js') }}"></script>
     @endpush
 
 @endif

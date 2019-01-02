84eba4dac6dffeed5481408fd70a7d62414a4ebe
Sami Greenbury <hello@mha.systems>
2019-01-02 16:03:34 +0000

    Dont escape labels in Show view

--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -49,7 +49,7 @@
 		        @foreach ($crud->columns as $column)
 		            <tr>
 		                <td>
-		                    <strong>{{ $column['label'] }}</strong>
+		                    <strong>{!! $column['label'] !!}</strong>
 		                </td>
                         <td>
 							@if (!isset($column['type']))

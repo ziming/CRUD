2e9aa2495a3d1d0b3f5abebbec774ec450e65727
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-02-27 14:28:53 +0200

    use Carbon/Carbon instead of jessengers/date to localize date and datetime columns

--- src/resources/views/columns/datetime.blade.php
+++ src/resources/views/columns/datetime.blade.php
@@ -1,10 +1,14 @@
-{{-- localized datetime using jenssegers/date --}}
+{{-- localized datetime using carbon --}}
 @php
     $value = data_get($entry, $column['name']);
 @endphp
 
 <span data-order="{{ $value }}">
     @if (!empty($value))
-	{{ Date::parse($value)->format(($column['format'] ?? config('backpack.base.default_datetime_format'))) }}
+	{{
+		\Carbon\Carbon::parse($value)
+		->locale(App::getLocale())
+		->isoFormat($column['format'] ?? config('backpack.base.default_datetime_format'))
+	}}
     @endif
 </span>

38d8dc53e01188e09ab205c948958f92fe2dbbd3
Athul Raj <mail@athulraj.com>
2019-03-29 12:08:19 +0530

    optional other fields in select2 ajax request

--- src/resources/views/fields/select2_from_ajax_multiple.blade.php
+++ src/resources/views/fields/select2_from_ajax_multiple.blade.php
@@ -80,7 +80,9 @@
                             return {
                                 q: params.term, // search term
                                 page: params.page, // pagination
-                                form: form.serializeArray() // all other form inputs
+                                @if($field['add_other_fields_in_ajax_request)'] ?? true)
+                                    form: form.serializeArray() // all other form inputs
+                                @endif
                             };
                         },
                         processResults: function (data, params) {

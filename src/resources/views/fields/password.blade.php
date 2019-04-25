58e9c7146be87547e4f0b27aecb529c73b4adf89
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-04-25 16:12:05 +0300

    Autocomplete off by default for the password field

--- src/resources/views/fields/password.blade.php
+++ src/resources/views/fields/password.blade.php
@@ -1,11 +1,18 @@
 <!-- password -->
+
+@php
+    // autocomplete off, if not otherwise specified
+    if (!isset($field['attributes']['autocomplete'])) {
+        $field['attributes']['autocomplete'] = "off";
+    }
+@endphp
+
 <div @include('crud::inc.field_wrapper_attributes') >
     <label>{!! $field['label'] !!}</label>
     @include('crud::inc.field_translatable_icon')
     <input
     	type="password"
     	name="{{ $field['name'] }}"
-    	autocomplete="new-password"
         @include('crud::inc.field_attributes')
     	>
 

67e3ba19a36c019bd254972893f1b3f7619aa690
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-05-09 11:37:06 +0300

    fixes #1591 - added storage disk and base64 support to image column type

--- src/resources/views/columns/image.blade.php
+++ src/resources/views/columns/image.blade.php
@@ -1,9 +1,19 @@
 {{-- image column type --}}
 @php
   $value = data_get($entry, $column['name']);
+  $prefix = $column['prefix'] ?? '';
 
   if (is_array($value)) {
     $value = json_encode($value);
+  }
+
+
+  if (preg_match('/^data\:image\//', $value)) { // base64_image
+    $href = $src = $value;
+  } elseif (isset($columns['disk'])) { // image from a different disk (like s3 bucket)
+    $href = $src = Storage::disk($column['disk'])->url($prefix.$value);
+  } else { // plain-old image, from a local disk
+    $href = $src = asset( $prefix . $value);
   }
 @endphp
 
@@ -11,13 +21,8 @@
   @if( empty($value) )
     -
   @else
-    <a
-      href="{{ asset( (isset($column['prefix']) ? $column['prefix'] : '') . $value) }}"
-      target="_blank"
-    >
-      <img
-        src="{{ asset( (isset($column['prefix']) ? $column['prefix'] : '') . $value) }}"
-        style="
+    <a href="{{ $href }}" target="_blank">
+      <img src="{{ $src }}" style="
           max-height: {{ isset($column['height']) ? $column['height'] : "25px" }};
           width: {{ isset($column['width']) ? $column['width'] : "auto" }};
           border-radius: 3px;"

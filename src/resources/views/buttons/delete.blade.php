32cad49cb2c0c54ab118d7760a06b07491c0f603
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-08-10 09:59:04 +0300

    much cuter delete modal with sweetalert instead of pnotify

--- src/resources/views/buttons/delete.blade.php
+++ src/resources/views/buttons/delete.blade.php
@@ -13,105 +13,76 @@
 		var route = button.attr('data-route');
 		var row = $("#crudTable a[data-route='"+route+"']").closest('tr');
 
-		var notice = PNotify.error({
-		  // title: 'Confirmation Needed',
-		  text: '{{ trans('backpack::crud.delete_confirm') }}<br><br>',
-		  textTrusted: true,
-		  icon: 'fa fa-question-circle',
-		  hide: false,
-		  stack: {
-		    'dir1': 'down',
-		    'modal': true,
-		    'firstpos1': 25
+		swal({
+		  title: "{{ trans('backpack::base.warning') }}!",
+		  text: "{{ trans('backpack::crud.delete_confirm') }}",
+		  icon: "warning",
+		  buttons: {
+		  	cancel: {
+			  text: "{{ trans('backpack::crud.cancel') }}",
+			  value: null,
+			  visible: true,
+			  className: "bg-secondary",
+			  closeModal: true,
+			},
+		  	delete: {
+			  text: "{{ trans('backpack::crud.delete') }}",
+			  value: true,
+			  visible: true,
+			  className: "bg-danger",
+			}
 		  },
-		  modules: {
-            // Animate: {
-            //   animate: true,
-            //   inClass: 'bounceIn',
-            //   outClass: 'bounceOut'
-            // },
-		    Confirm: {
-		      confirm: true,
-		      align: 'center',
-		      buttons: [{
-		          text: '{{ trans('backpack::crud.delete') }}',
-		          primary: true,
-		          click: function(notice) {
-		            $.ajax({
-					      url: route,
-					      type: 'DELETE',
-					      success: function(result) {
-					          if (result != 1) {
-					          	// Show an error alert
-					              PNotify.alert({
-					                  title: "{{ trans('backpack::crud.delete_confirmation_not_title') }}",
-					                  text: "{{ trans('backpack::crud.delete_confirmation_not_message') }}",
-					                  type: "notice"
-					              });
-					          } else {
-					              // Show a success alert with the result
-					              PNotify.alert({
-					                  title: "{{ trans('backpack::crud.delete_confirmation_title') }}",
-					                  text: "{{ trans('backpack::crud.delete_confirmation_message') }}",
-					                  type: "success"
-					              });
+		}).then((value) => {
+			if (value) {
+				$.ajax({
+			      url: route,
+			      type: 'DELETE',
+			      success: function(result) {
+			          if (result != 1) {
+			          	// Show an error alert
+			              swal({
+			              	title: "{{ trans('backpack::crud.delete_confirmation_not_title') }}",
+			              	text: "{{ trans('backpack::crud.delete_confirmation_not_message') }}",
+			              	icon: "error",
+			              	timer: 2000,
+			              	buttons: false,
+			              });
+			          } else {
+			          	  // Show a success message
+			              swal({
+			              	title: "{{ trans('backpack::crud.delete_confirmation_title') }}",
+			              	text: "{{ trans('backpack::crud.delete_confirmation_message') }}",
+			              	icon: "success",
+			              	timer: 2000,
+			              	buttons: false,
+			              });
 
-					              // Hide the modal, if any
-					              $('.modal').modal('hide');
+			              // Hide the modal, if any
+			              $('.modal').modal('hide');
 
-					              // Remove the details row, if it is open
-					              if (row.hasClass("shown")) {
-					                  row.next().remove();
-					              }
+			              // Remove the details row, if it is open
+			              if (row.hasClass("shown")) {
+			                  row.next().remove();
+			              }
 
-					              // Remove the row from the datatable
-					              row.remove();
-					          }
-					      },
-					      error: function(result) {
-					          // Show an alert with the result
-					          PNotify.alert({
-					              title: "{{ trans('backpack::crud.delete_confirmation_not_title') }}",
-					              text: "{{ trans('backpack::crud.delete_confirmation_not_message') }}",
-					              type: "notice"
-					          });
-					      }
-					  });
-		          }
-		        },
-		        {
-		          text: '{{ trans('backpack::crud.cancel') }}',
-		          click: function(notice) {
-		            notice.update({
-		              title: '{{ trans('backpack::crud.delete_confirmation_not_deleted_title') }}',
-		              text: '{{ trans('backpack::crud.delete_confirmation_not_deleted_message') }}',
-		              icon: true,
-		              type: 'info',
-		              delay: '1000',
-		              hide: true,
-		              modules: {
-		                Confirm: {
-		                  confirm: false
-		                },
-		                Buttons: {
-		                  closer: true,
-		                  sticker: true
-		                }
-		              }
-		            });
-		          }
-		        }
-		      ]
-		    },
-		    Buttons: {
-		      closer: false,
-		      sticker: false
-		    },
-		    History: {
-		      history: false
-		    }
-		  }
+			              // Remove the row from the datatable
+			              row.remove();
+			          }
+			      },
+			      error: function(result) {
+			          // Show an alert with the result
+			          swal({
+		              	title: "{{ trans('backpack::crud.delete_confirmation_not_title') }}",
+		              	text: "{{ trans('backpack::crud.delete_confirmation_not_message') }}",
+		              	icon: "error",
+		              	timer: 2000,
+		              	buttons: false,
+		              });
+			      }
+			  });
+			}
 		});
+
       }
 	}
 

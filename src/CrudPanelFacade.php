0acec668960c839687b6d895826f60c095d61294
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-08-29 12:58:28 +0300

    added comments

--- src/CrudPanelFacade.php
+++ src/CrudPanelFacade.php
@@ -4,6 +4,11 @@ namespace Backpack\CRUD;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * This object allows developers to use CRUD::addField() instead of $this->crud->addField(),
+ * by providing a Facade that leads to the CrudPanel object. That object is stored in Laravel's
+ * service container as 'crud'. 
+ */
 class CrudPanelFacade extends Facade
 {
     /**

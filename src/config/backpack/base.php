b41fd7faf1e0ee4a6386501fb058a02a6fec1f34
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-05 10:46:00 +0300

    Update base.php

--- src/config/backpack/base.php
+++ src/config/backpack/base.php
@@ -123,7 +123,7 @@ return [
     // All JS and CSS assets defined above have this string appended as query string (?v=string).
     // If you want to manually trigger cachebusting for all styles and scripts,
     // append or prepent something to the string below, so that it's different.
-    'cachebusting_string' => \PackageVersions\Versions::getVersion('backpack/base'),
+    'cachebusting_string' => \PackageVersions\Versions::getVersion('backpack/crud'),
 
     /*
     |--------------------------------------------------------------------------

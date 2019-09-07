e5a58acf99f7d9eb964ead33e341da751607fb08
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-07 09:37:19 +0300

    browse_multiple field javascript separation

--- src/resources/views/crud/fields/browse_multiple.blade.php
+++ src/resources/views/crud/fields/browse_multiple.blade.php
@@ -5,6 +5,23 @@ $value = old(square_brackets_to_dots($field['name'])) ?? $field['value'] ?? $fie
 if (!$multiple && is_array($value)) {
     $value = array_first($value);
 }
+
+if (!isset($field['wrapperAttributes']) || !isset($field['wrapperAttributes']['data-init-function']))
+{
+    $field['wrapperAttributes']['data-init-function'] = 'bpFieldInitBrowseMultipleElement';
+    
+    if ($multiple) {
+        $field['wrapperAttributes']['data-popup-title'] = trans('backpack::crud.select_files');
+        $field['wrapperAttributes']['data-multiple'] = "true";
+    } else {
+        $field['wrapperAttributes']['data-popup-title'] = trans('backpack::crud.select_file');
+        $field['wrapperAttributes']['data-multiple'] = "false";
+    }
+
+    if ($mimes = array_get($field, 'mime_types')) {
+        $field['wrapperAttributes']['data-only-mimes'] = json_encode($mimes);
+    }
+}
 @endphp
 
 <div @include('crud::inc.field_wrapper_attributes') >
@@ -12,12 +29,13 @@ if (!$multiple && is_array($value)) {
     <div><label>{!! $field['label'] !!}</label></div>
     @include('crud::inc.field_translatable_icon')
     @if ($multiple)
-        @foreach((array)$value as $v)
+        @foreach( (array)$value as $v)
             @if ($v)
                 <div class="input-group input-group-sm">
-                    <input type="text" name="{{ $field['name'] }}[]" value="{{ $v }}" @include('crud::inc.field_attributes') readonly>
+                    <input type="text" name="{{ $field['name'] }}[]" value="{{ $v }}" data-marker="multipleBrowseInput"
+                            @include('crud::inc.field_attributes') readonly>
                     <div class="input-group-btn">
-                        <button type="button" class="browse_{{ $field['name'] }} remove btn btn-sm btn-light">
+                        <button type="button" class="browse remove btn btn-sm btn-light">
                             <i class="fa fa-trash"></i>
                         </button>
                     </div>
@@ -29,11 +47,11 @@ if (!$multiple && is_array($value)) {
     @endif
 
     <div class="btn-group" role="group" aria-label="..." style="margin-top: 3px;">
-        <button type="button" class="browse_{{ $field['name'] }} popup btn btn-sm btn-light">
+        <button type="button" class="browse popup btn btn-sm btn-light">
             <i class="fa fa-cloud-upload"></i>
             {{ trans('backpack::crud.browse_uploads') }}
         </button>
-        <button type="button" class="browse_{{ $field['name'] }} clear btn btn-sm btn-light">
+        <button type="button" class="browse clear btn btn-sm btn-light">
             <i class="fa fa-eraser"></i>
             {{ trans('backpack::crud.clear') }}
         </button>
@@ -43,18 +61,18 @@ if (!$multiple && is_array($value)) {
         <p class="help-block">{!! $field['hint'] !!}</p>
     @endif
 
+    <script type="text/html" data-marker="browse_multiple_template">
+        <div class="input-group input-group-sm">
+            <input type="text" name="{{ $field['name'] }}[]" @include('crud::inc.field_attributes') readonly>
+            <div class="input-group-btn">
+                <button type="button" class="browse remove btn btn-sm btn-light">
+                    <i class="fa fa-trash"></i>
+                </button>
+            </div>
+        </div>
+    </script>
 </div>
 
-<script type="text/html" id="browse_multiple_template_{{ $field['name'] }}">
-    <div class="input-group input-group-sm">
-        <input type="text" name="{{ $field['name'] }}[]" @include('crud::inc.field_attributes') readonly>
-        <div class="input-group-btn">
-            <button type="button" class="browse_{{ $field['name'] }} remove btn btn-sm btn-light">
-                <i class="fa fa-trash"></i>
-            </button>
-        </div>
-    </div>
-</script>
 
 {{-- ########################################## --}}
 {{-- Extra CSS and JS for this particular field --}}
@@ -84,94 +102,90 @@ if (!$multiple && is_array($value)) {
         <script src="{{ asset('packages/jquery-colorbox/jquery.colorbox-min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('packages/barryvdh/elfinder/js/elfinder.min.js') }}"></script>
         {{-- <script type="text/javascript" src="{{ asset('packages/barryvdh/elfinder/js/extras/editors.default.min.js') }}"></script> --}}
-        @if (($locale = \App::getLocale()) != 'en')
+        @if ( ($locale = \App::getLocale()) != 'en' )
+            @php
             <script type="text/javascript" src="{{ asset("packages/barryvdh/elfinder/js/i18n/elfinder.{$locale}.js") }}"></script>
         @endif
+
+        <script>
+            function bpFieldInitBrowseMultipleElement(element) {
+                var $template = element.find("[data-marker=browse_multiple_template]").html();
+                var $popupButton = element.find(".popup");
+                var $clearButton = element.find(".clear");
+                var $removeButton = element.find(".remove");
+                var $input = element.find('input[data-marker=multipleBrowseInput]');
+                var $popupTitle = element.attr('data-popup-title');
+                var $onlyMimesArray = element.attr('data-only-mimes');
+                var $multiple = element.attr('data-multiple');
+
+                element.on('click', 'button.popup', function (event) {
+                    event.preventDefault();
+
+                    var div = $('<div>');
+                    div.elfinder({
+                        lang: '{{ \App::getLocale() }}',
+                        customData: {
+                            _token: '{{ csrf_token() }}'
+                        },
+                        url: '{{ route("elfinder.connector") }}',
+                        soundPath: '{{ asset('/packages/barryvdh/elfinder/sounds') }}',
+                        dialog: {
+                            width: 900,
+                            modal: true,
+                            title: $popupTitle,
+                        },
+                        resizable: false,
+                        onlyMimes: $onlyMimesArray,
+                        commandsOptions: {
+                            getfile: {
+                                multiple: $multiple,
+                                oncomplete: 'destroy'
+                            }
+                        },
+                        getFileCallback: function (files) {
+                            if ($multiple) {
+                                files.forEach(function (file) {
+                                    var newInput = $($template);
+                                    newInput.find('input').val(file.path);
+                                    $popupButton.parent().before(newInput);
+                                });
+                            } else {
+                                $input.val(files.path);
+                            }
+                            
+                            $.colorbox.close();
+                        }
+                    }).elfinder('instance');
+
+                    // trigger the reveal modal with elfinder inside
+                    $.colorbox({
+                        href: div,
+                        inline: true,
+                        width: '80%',
+                        height: '80%'
+                    });
+                });
+
+                element.on('click', 'button.clear', function (event) {
+                    event.preventDefault();
+
+                    if ($multiple) {
+                        $input.parents('.input-group').remove();    
+                    } else {
+                        $input.val('');
+                    }
+                });
+
+                if ($multiple) {
+                    element.on('click', 'button.remove', function (event) {
+                        event.preventDefault();
+                        $(this).parents('.input-group').remove();
+                    });
+                }
+            }
+        </script>
     @endpush
 @endif
-
-{{-- FIELD JS - will be loaded in the after_scripts section --}}
-@push('crud_fields_scripts')
-    <script>
-        $(function () {
-            var template = document.getElementById('browse_multiple_template_{{ $field['name'] }}').innerHTML;
-
-            $(document).on('click', '.popup.browse_{{ $field['name'] }}', function (event) {
-                event.preventDefault();
-
-                var element = $(this);
-
-                var div = $('<div>');
-                div.elfinder({
-                    lang: '{{ \App::getLocale() }}',
-                    customData: {
-                        _token: '{{ csrf_token() }}'
-                    },
-                    url: '{{ route("elfinder.connector") }}',
-                    soundPath: '{{ asset('/packages/barryvdh/elfinder/sounds') }}',
-                    dialog: {
-                        width: 900,
-                        modal: true,
-                        @if ($multiple)
-                        title: '{{ trans('backpack::crud.select_files') }}',
-                        @else
-                        title: '{{ trans('backpack::crud.select_file') }}',
-                        @endif
-                    },
-                    resizable: false,
-                    @if ($mimes = array_get($field, 'mime_types'))
-                    onlyMimes: {!! json_encode($mimes) !!},
-                    @endif
-                    commandsOptions: {
-                        getfile: {
-                            @if ($multiple)
-                            multiple: true,
-                            @endif
-                            oncomplete: 'destroy'
-                        }
-                    },
-                    getFileCallback: function (files) {
-                        @if ($multiple)
-                        files.forEach(function (file) {
-                            var input = $(template);
-                            input.find('input').val(file.path);
-                            element.parent().before(input);
-                        });
-                        @else
-                        $('input[name=\'{{ $field['name'] }}\']').val(files.path);
-                        @endif
-                        $.colorbox.close();
-                    }
-                }).elfinder('instance');
-
-                // trigger the reveal modal with elfinder inside
-                $.colorbox({
-                    href: div,
-                    inline: true,
-                    width: '80%',
-                    height: '80%'
-                });
-            });
-
-            $(document).on('click', '.clear.browse_{{ $field['name'] }}', function (event) {
-                event.preventDefault();
-
-                @if ($multiple)
-                $('input[name=\'{{ $field['name'] }}[]\']').parents('.input-group').remove();
-                @else
-                $('input[name=\'{{ $field['name'] }}\']').val('');
-                @endif
-            });
-
-            @if ($multiple)
-            $(document).on('click', '.remove.browse_{{ $field['name'] }}', function (event) {
-                event.preventDefault();
-                $(this).parents('.input-group').remove();
-            });
-            @endif
-        });
-    </script>
-@endpush
 
 {{-- End of Extra CSS and JS --}}
 {{-- ########################################## --}}

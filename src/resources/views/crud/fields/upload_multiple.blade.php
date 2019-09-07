62b420501c6a081a55dbb9f9079c40d440964b7f
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-07 11:19:31 +0300

    upload_multiple field javascript separation

--- src/resources/views/crud/fields/upload_multiple.blade.php
+++ src/resources/views/crud/fields/upload_multiple.blade.php
@@ -1,3 +1,11 @@
+@php
+    if (!isset($field['wrapperAttributes']) || !isset($field['wrapperAttributes']['class']))
+    {
+        $field['wrapperAttributes']['data-init-function'] = 'bpFieldInitUploadMultipleElement';
+        $field['wrapperAttributes']['data-field-name'] = $field['name'];
+    }
+@endphp
+
 <!-- upload multiple input -->
 <div @include('crud::inc.field_wrapper_attributes') >
     <label>{!! $field['label'] !!}</label>
@@ -13,7 +21,7 @@
 		}
 	@endphp
 	@if (count($values))
-    <div class="well well-sm file-preview-container">
+    <div class="well well-sm existing-file">
     	@foreach($values as $key => $file_path)
     		<div class="file-preview">
     			@if (isset($field['temporary']))
@@ -21,7 +29,7 @@
 		        @else
 		            <a target="_blank" href="{{ isset($field['disk'])?asset(\Storage::disk($field['disk'])->url($file_path)):asset($file_path) }}">{{ $file_path }}</a>
 		        @endif
-		    	<a id="{{ $field['name'] }}_{{ $key }}_clear_button" href="#" class="btn btn-default btn-xs pull-right file-clear-button" title="Clear file" data-filename="{{ $file_path }}"><i class="fa fa-remove"></i></a>
+		    	<a href="#" class="btn btn-light btn-sm float-right file-clear-button" title="Clear file" data-filename="{{ $file_path }}"><i class="fa fa-remove"></i></a>
 		    	<div class="clearfix"></div>
 	    	</div>
     	@endforeach
@@ -30,14 +38,16 @@
     @endif
 	{{-- Show the file picker on CREATE form. --}}
 	<input name="{{ $field['name'] }}[]" type="hidden" value="">
-	<input
-        type="file"
-        id="{{ $field['name'] }}_file_input"
-        name="{{ $field['name'] }}[]"
-        value="@if (old(square_brackets_to_dots($field['name']))) old(square_brackets_to_dots($field['name'])) @elseif (isset($field['default'])) $field['default'] @endif"
-        @include('crud::inc.field_attributes')
-        multiple
-    >
+	<div class="backstrap-file mt-2">
+		<input
+	        type="file"
+	        name="{{ $field['name'] }}[]"
+	        value="@if (old(square_brackets_to_dots($field['name']))) old(square_brackets_to_dots($field['name'])) @elseif (isset($field['default'])) $field['default'] @endif"
+	        @include('crud::inc.field_attributes', ['default_class' =>  isset($field['value']) && $field['value']!=null?'file_input backstrap-file-input':'file_input backstrap-file-input'])
+	        multiple
+	    >
+        <label class="backstrap-file-label" for="customFile"></label>
+    </div>
 
     {{-- HINT --}}
     @if (isset($field['hint']))
@@ -45,29 +55,42 @@
     @endif
 </div>
 
-{{-- FIELD EXTRA JS --}}
-{{-- push things in the after_scripts section --}}
+{{-- ########################################## --}}
+{{-- Extra CSS and JS for this particular field --}}
+{{-- If a field type is shown multiple times on a form, the CSS and JS will only be loaded once --}}
+@if ($crud->fieldTypeNotLoaded($field))
+    @php
+        $crud->markFieldTypeAsLoaded($field);
+    @endphp
 
     @push('crud_fields_scripts')
         <!-- no scripts -->
         <script>
-	        $(".file-clear-button").click(function(e) {
-	        	e.preventDefault();
-	        	var container = $(this).parent().parent();
-	        	var parent = $(this).parent();
-	        	// remove the filename and button
-	        	parent.remove();
-	        	// if the file container is empty, remove it
-	        	if ($.trim(container.html())=='') {
-	        		container.remove();
-	        	}
-	        	$("<input type='hidden' name='clear_{{ $field['name'] }}[]' value='"+$(this).data('filename')+"'>").insertAfter("#{{ $field['name'] }}_file_input");
-	        });
+        	function bpFieldInitUploadMultipleElement(element) {
+        		var fieldName = element.attr('data-field-name');
+        		var clearFileButton = element.find(".file-clear-button");
+        		var fileInput = element.find("input[type=file]");
+        		var inputLabel = element.find("label.backstrap-file-label");
 
-	        $("#{{ $field['name'] }}_file_input").change(function() {
-	        	console.log($(this).val());
-	        	// remove the hidden input, so that the setXAttribute method is no longer triggered
-	        	$(this).next("input[type=hidden]").remove();
-	        });
+		        clearFileButton.click(function(e) {
+		        	e.preventDefault();
+		        	var container = $(this).parent().parent();
+		        	var parent = $(this).parent();
+		        	// remove the filename and button
+		        	parent.remove();
+		        	// if the file container is empty, remove it
+		        	if ($.trim(container.html())=='') {
+		        		container.remove();
+		        	}
+		        	$("<input type='hidden' name='clear_"+fieldName+"[]' value='"+$(this).data('filename')+"'>").insertAfter(fileInput);
+		        });
+
+		        fileInput.change(function() {
+	                inputLabel.html("Files selected. After save, they will show up above.");
+		        	// remove the hidden input, so that the setXAttribute method is no longer triggered
+		        	$(this).next("input[type=hidden]").remove();
+		        });
+        	}
         </script>
     @endpush
+@endif

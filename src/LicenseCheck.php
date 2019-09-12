0703c0d494ef30a6871c9f65f07445daf04efbd1
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-12 11:37:25 +0300

    fixes #2046 - no license check in console and unit tests

--- src/LicenseCheck.php
+++ src/LicenseCheck.php
@@ -12,6 +12,12 @@ trait LicenseCheck
      */
     private function checkLicenseCodeExists()
     {
+        // don't even check if it's a console command or unit tests
+        if ($this->app->runningInConsole() || $this->app->runningUnitTests()) 
+        {
+            return;
+        }
+
         // don't show notice bubble on localhost
         if (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
             return;

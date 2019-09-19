9cf0fa2e8484f68a97bd32b0b1c561c5a18da941
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-19 08:45:38 +0300

    fix autoset not to add a column if it's already defined

--- src/app/Library/CrudPanel/Traits/AutoSet.php
+++ src/app/Library/CrudPanel/Traits/AutoSet.php
@@ -33,7 +33,7 @@ trait AutoSet
                 $this->addField($new_field);
             }
 
-            if (! in_array($field, $this->model->getHidden()) && ! isset($this->columns[$field])) {
+            if (! in_array($field, $this->model->getHidden()) && ! in_array($field, $this->columns())) {
                 $this->addColumn([
                     'name'    => $field,
                     'label'   => $this->makeLabel($field),

76c7741dfdcb027fb05a2683a7e45fe8f3818104
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-20 09:07:09 +0300

    blue dropdown carets

--- src/resources/views/crud/buttons/show.blade.php
+++ src/resources/views/crud/buttons/show.blade.php
@@ -9,7 +9,7 @@
 	<!-- Edit button group -->
 	<div class="btn-group">
 	  <a href="{{ url($crud->route.'/'.$entry->getKey().'/show') }}" class="btn btn-sm btn-link pr-0"><i class="fa fa-eye"></i> {{ trans('backpack::crud.preview') }}</a>
-	  <a class="btn btn-sm btn-link dropdown-toggle pl-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	  <a class="btn btn-sm btn-link dropdown-toggle text-primary pl-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 	    <span class="caret"></span>
 	  </a>
 	  <ul class="dropdown-menu dropdown-menu-right">

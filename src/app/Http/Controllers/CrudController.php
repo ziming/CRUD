6a191a25df833b5449f455bbc7f542884144e50c
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-28 11:15:23 +0300

    Apply fixes from StyleCI (#2092)
    [ci skip] [skip ci]

--- src/app/Http/Controllers/CrudController.php
+++ src/app/Http/Controllers/CrudController.php
@@ -92,20 +92,20 @@ class CrudController extends Controller
         $operationName = $this->crud->getCurrentOperation();
         $setupClassName = 'setup'.studly_case($operationName).'Operation';
 
-        /**
+        /*
          * FIRST, run all Operation Closures for this operation.
-         * 
-         * It's preferred for this to closures first, because 
+         *
+         * It's preferred for this to closures first, because
          * (1) setup() is usually higher in a controller than any other method, so it's more intuitive,
          * since the first thing you write is the first thing that is being run;
-         * (2) operations use operation closures themselves, inside their setupXxxDefaults(), and 
+         * (2) operations use operation closures themselves, inside their setupXxxDefaults(), and
          * you'd like the defaults to be applied before anything you write. That way, anything you
          * write is done after the default, so you can remove default settings, etc;
          */
         $this->crud->applyConfigurationFromSettings($operationName);
 
-        /**
-         * THEN, run the corresponding setupXxxOperation if it exists. 
+        /*
+         * THEN, run the corresponding setupXxxOperation if it exists.
          */
         if (method_exists($this, $setupClassName)) {
             $this->{$setupClassName}();

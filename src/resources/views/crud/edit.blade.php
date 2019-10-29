84850eb44418477622e8882e17816009faad2b1e
unknown <DiyanDesigns@gmail.com>
2019-10-29 12:37:26 +0200

    [4.0][Bug] Fixed the language switch float

--- src/resources/views/crud/edit.blade.php
+++ src/resources/views/crud/edit.blade.php
@@ -41,9 +41,9 @@
 		  {!! method_field('PUT') !!}
 
 		  	@if ($crud->model->translationEnabled())
-		    <div class="mb-2">
+		    <div class="mb-2 text-right">
 		    	<!-- Single button -->
-				<div class="btn-group float-right">
+				<div class="btn-group">
 				  <button type="button" class="btn btn-sm btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				    {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} &nbsp; <span class="caret"></span>
 				  </button>

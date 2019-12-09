771023b1887258f016c3b02aa0c79ee27d06f95d
Pedro Martins <pxpm88@gmail.com>
2019-12-09 17:24:54 +0000

    Dont throw error if column does not exists in DB

--- src/app/Models/Traits/CrudTrait.php
+++ src/app/Models/Traits/CrudTrait.php
@@ -68,8 +68,13 @@ trait CrudTrait
         $conn->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
         $conn->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('json', 'json_array');
         $conn->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('jsonb', 'json_array');
-
-        return ! $conn->getDoctrineColumn($table, $column_name)->getNotnull();
+        try {
+            //check if column exists in database
+            $conn->getDoctrineColumn($table, $column_name);
+            return ! $conn->getDoctrineColumn($table, $column_name)->getNotnull();
+        } catch (Exception $e) {
+            return true;
+        }
     }
 
     /*

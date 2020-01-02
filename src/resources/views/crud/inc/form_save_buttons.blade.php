ead3417cf3d1fbdab029b5446a4876c88904b7ce
Pedro Martins <pxpm88@gmail.com>
2020-01-02 13:14:50 +0000

    Fixes #2339
    When selecting any option from save actions dropdown the html5 validation was not running.

--- src/resources/views/crud/inc/form_save_buttons.blade.php
+++ src/resources/views/crud/inc/form_save_buttons.blade.php
@@ -13,7 +13,7 @@
             <button id="btnGroupDrop1" type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="caret"></span><span class="sr-only">&#x25BC;</span></button>
             <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
                 @foreach( $saveAction['options'] as $value => $label)
-                <a class="dropdown-item" href="javascript:void(0);" data-value="{{ $value }}">{{ $label }}</a>
+                <a class="dropdown-item" href="#" data-value="{{ $value }}">{{ $label }}</a>
                 @endforeach
             </div>
           </div>
@@ -22,3 +22,26 @@
 
     <a href="{{ $crud->hasAccess('list') ? url($crud->route) : url()->previous() }}" class="btn btn-default"><span class="fa fa-ban"></span> &nbsp;{{ trans('backpack::crud.cancel') }}</a>
 </div>
+
+@push('after_scripts')
+<script>
+
+     jQuery(document).ready(function($) {
+     var selector = $('#btnGroupDrop1').next();
+        $(selector).find('a').each(function() {
+            $(this).click(function(e) {
+                e.stopPropagation()
+                let form = $(this).closest('form');
+                if (!form[0].checkValidity || form[0].checkValidity()) {
+                    form.submit();
+                }else{
+                    if (form[0].reportValidity) {
+                        form[0].reportValidity();
+                    }
+                }
+            });
+        });
+    });
+
+    </script>
+@endpush

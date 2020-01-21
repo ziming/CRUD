d534555e5f16e04b264fa6314ed0bab60b26ae0b
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-01-21 13:30:08 +0200

    smaller print button; footer hidden on print

--- src/resources/views/crud/show.blade.php
+++ src/resources/views/crud/show.blade.php
@@ -13,14 +13,14 @@
 
 @section('header')
 	<section class="container-fluid d-print-none">
-	 <h2>
-        <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
-        <small>{!! $crud->getSubheading() ?? mb_ucfirst(trans('backpack::crud.preview')).' '.$crud->entity_name !!}.</small>
-        @if ($crud->hasAccess('list'))
-          <small class=""><a href="{{ url($crud->route) }}" class="font-sm"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a></small>
-        @endif
-        <a href="javascript: window.print();" class="float-right"><i class="fa fa-print"></i></a>
-     </h2>
+    	<a href="javascript: window.print();" class="btn float-right"><i class="fa fa-print"></i></a>
+		<h2>
+	        <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
+	        <small>{!! $crud->getSubheading() ?? mb_ucfirst(trans('backpack::crud.preview')).' '.$crud->entity_name !!}.</small>
+	        @if ($crud->hasAccess('list'))
+	          <small class=""><a href="{{ url($crud->route) }}" class="font-sm"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a></small>
+	        @endif
+	    </h2>
     </section>
 @endsection
 

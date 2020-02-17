307135a1891508963c1688944d9ed029f8354a83
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-02-17 11:36:20 +0200

    upgraded phpunit to v8

--- tests/Unit/CrudPanel/BaseDBCrudPanelTest.php
+++ tests/Unit/CrudPanel/BaseDBCrudPanelTest.php
@@ -26,7 +26,7 @@ abstract class BaseDBCrudPanelTest extends BaseCrudPanelTest
         // call migrations specific to our tests
         $this->loadMigrationsFrom([
             '--database' => 'testing',
-            '--realpath' => realpath(__DIR__.'/../../config/database/migrations'),
+            '--path' => realpath(__DIR__.'/../../config/database/migrations'),
         ]);
 
         $this->artisan('db:seed', ['--class' => 'UsersRolesTableSeeder']);

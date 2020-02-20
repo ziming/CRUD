b3ab7c625ae60cef731177d0e03769a89d67c679
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-02-20 12:45:17 +0200

    indentation and full_screen_loader spinner size

--- src/resources/views/crud/fields/relationship/create_button.blade.php
+++ src/resources/views/crud/fields/relationship/create_button.blade.php
@@ -1,5 +1,3 @@
-
-
 <button type="button" class="btn btn-sm btn-link float-right" id="{{ $inlineCreateEntity }}-inline-create-{{$name}}"><span class="fa fa-plus"></span>{{trans('backpack::crud.add')}}</span></button>
-@include(backpack_view('inc.full_screen_loader'))
 
+@include(backpack_view('inc.full_screen_loader'))

4cc9d9511cdc971a7da5e801546ed86d5de300cb
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-03-02 19:46:51 +0200

    light code cleanup

--- src/resources/views/crud/columns/inc/column_wrapper.blade.php
+++ src/resources/views/crud/columns/inc/column_wrapper.blade.php
@@ -1,21 +1,21 @@
 @php
-    //this is passed by columns like select and select_multiple
+    // this is passed by columns like select and select_multiple
     $related_model_key = $related_model_key ?? null;
 
-    //if the wrapper attribute is callable we run the callback
+    // each wrapper attribute can be a callback or a string
+    // for those that are callbacks, run the callbacks to get the final string to use
     foreach($column['wrapper'] as $attribute => $value) {
-        $attributeValue = is_callable($value) ? $value($crud, $column, $entry, $related_model_key) : ($value ?? '');
-        $column['wrapper'][$attribute] = $attributeValue;
+        $column['wrapper'][$attribute] = is_callable($value) ? $value($crud, $column, $entry, $related_model_key) : $value ?? '';
     }
 
-    //setup defaults if wrapper is not empty, case empty we just want to display the text.
+    // if the wrapper "element" is not defined, set it to the default - an anchor
     if(!empty($column['wrapper'])) {
         $column['wrapper']['element'] = $column['wrapper']['element'] ?? 'a';
     }
 
-    //we need this $text variable in case column is a select, because we include the wrapper in
-    //all selected entries and not only once in the column.
-    $column['text'] = $text ?? ($column['text'] ?? '-');
+    // we need this $text variable in case column is a select, because we include the wrapper in
+    // all selected entries and not only once in the column.
+    $column['text'] = $text ?? $column['text'] ?? '-';
 
 @endphp
 

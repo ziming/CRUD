df11f9986eeea43822686ff0cda3455405e2a1ac
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-03-06 09:04:51 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/app/Library/CrudPanel/Traits/Columns.php
+++ src/app/Library/CrudPanel/Traits/Columns.php
@@ -381,27 +381,29 @@ trait Columns
 
     /**
      * Check if a column exists, by any given attribute.
-     * 
+     *
      * @param  string  $attribute   Attribute name on that column definition array.
      * @param  string  $value       Value of that attribute on that column definition array.
-     * @return boolean
+     * @return bool
      */
-    public function hasColumnWhere($attribute, $value) {
+    public function hasColumnWhere($attribute, $value)
+    {
         $match = Arr::first($this->columns(), function ($column, $columnKey) use ($attribute, $value) {
             return isset($column[$attribute]) && $column[$attribute] == $value;
         });
 
-        return (bool)$match;
+        return (bool) $match;
     }
 
     /**
      * Get the first column where a given attribute has the given value.
-     * 
+     *
      * @param  string  $attribute   Attribute name on that column definition array.
      * @param  string  $value       Value of that attribute on that column definition array.
-     * @return boolean
+     * @return bool
      */
-    public function firstColumnWhere($attribute, $value) {
+    public function firstColumnWhere($attribute, $value)
+    {
         return Arr::first($this->columns(), function ($column, $columnKey) use ($attribute, $value) {
             return isset($column[$attribute]) && $column[$attribute] == $value;
         });
@@ -456,7 +458,6 @@ trait Columns
         return $this;
     }
 
-
     /**
      * Create and return a CrudColumn object for that column name.
      *
@@ -467,7 +468,7 @@ trait Columns
      *
      * And if the developer uses the CrudColumn object as Column in his CrudController:
      * - Column::name('price')->type('number');
-     * 
+     *
      * @param  string $name [description]
      * @return CrudColumn
      */

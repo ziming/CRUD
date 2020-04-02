8afcb6e0123f24c1cc332d33d0a3dab74dd65cc2
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-04-02 12:12:06 +0300

    added backwards-compatible wrapper to widgets

--- src/resources/views/base/widgets/chart.blade.php
+++ src/resources/views/base/widgets/chart.blade.php
@@ -10,9 +10,12 @@
   $controller = new $widget['controller'];
   $chart = $controller->chart;
   $path = $controller->getLibraryFilePath();
+
+  // defaults
+  $widget['wrapper']['class'] = $widget['wrapperClass'] ?? 'col-sm-6 col-md-4';
 @endphp
 
-<div class="{{ $widget['wrapperClass'] ?? 'col-sm-6 col-md-4' }}">
+@includeWhen(!empty($widget['wrapper']), 'backpack::widgets.inc.wrapper_start')
   <div class="{{ $widget['class'] ?? 'card mb-2' }}">
     @if (isset($widget['content']['header']))
     <div class="card-header">{!! $widget['content']['header'] !!}</div>
@@ -27,7 +30,7 @@
 
     </div>
   </div>
-</div>
+@includeWhen(!empty($widget['wrapper']), 'backpack::widgets.inc.wrapper_end')
 
 @push('after_scripts')
   @if (is_array($path))
--- src/resources/views/base/widgets/view.blade.php
+++ src/resources/views/base/widgets/view.blade.php
@@ -1,2 +1,6 @@
 <!-- view field -->
-@include($widget['view'], ['widget' => $widget])
+@includeWhen(!empty($widget['wrapper']), 'backpack::widgets.inc.wrapper_start')
+	
+	@include($widget['view'], ['widget' => $widget])
+
+@includeWhen(!empty($widget['wrapper']), 'backpack::widgets.inc.wrapper_end')

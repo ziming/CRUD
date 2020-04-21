a1fdccccc422a9826dfabe6b23ccbaa9f0550be1
martijnb92 <martijn.vanderburg@gmail.com>
2020-04-21 16:12:25 +0200

    Date range calendar Icon
    Glyphicons are no longer available.

--- src/resources/views/crud/fields/date_range.blade.php
+++ src/resources/views/crud/fields/date_range.blade.php
@@ -42,8 +42,10 @@
             type="text"
             @include('crud::inc.field_attributes')
             >
-        <div class="input-group-addon">
-            <span class="glyphicon glyphicon-calendar"></span>
+        	<div class="input-group-append">
+	            <span class="input-group-text">
+                <span class="fa fa-calendar"></span>
+            </span>
         </div>
     </div>
 

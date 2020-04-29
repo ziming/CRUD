32a7f9476bb1aab1f2f6b3db04d9c606b91c934c
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-04-29 08:32:05 +0300

    addresses #2266 - radio field now works inside repeatable

--- src/resources/views/crud/fields/radio.blade.php
+++ src/resources/views/crud/fields/radio.blade.php
@@ -1,12 +1,13 @@
 <!-- radio -->
 @php
-    $optionPointer = 0;
     $optionValue = old(square_brackets_to_dots($field['name'])) ?? $field['value'] ?? $field['default'] ?? '';
 
     // if the class isn't overwritten, use 'radio'
     if (!isset($field['attributes']['class'])) {
         $field['attributes']['class'] = 'radio';
     }
+
+    $field['wrapper']['data-init-function'] = $field['wrapper']['data-init-function'] ?? 'bpFieldInitRadioElement';
 @endphp
 
 @include('crud::fields.inc.wrapper_start')
@@ -16,39 +17,20 @@
         @include('crud::fields.inc.translatable_icon')
     </div>
 
+    <input type="hidden" value="{{ $optionValue }}" name="{{$field['name']}}" />
+
     @if( isset($field['options']) && $field['options'] = (array)$field['options'] )
 
         @foreach ($field['options'] as $value => $label )
-            @php ($optionPointer++)
 
-            @if( isset($field['inline']) && $field['inline'] )
-
-            <div class="form-check form-check-inline">
+            <div class="form-check {{ isset($field['inline']) && $field['inline'] ? 'form-check-inline' : '' }}">
                 <input  type="radio"
                         class="form-check-input"
-                        id="{{$field['name']}}_{{$optionPointer}}"
-                        name="{{$field['name']}}"
                         value="{{$value}}"
                         @include('crud::fields.inc.attributes')
-                        {{$optionValue == $value ? ' checked': ''}}
                         >
-                <label class="radio-inline form-check-label font-weight-normal" for="{{$field['name']}}_{{$optionPointer}}">{!! $label !!}</label>
+                <label class="{{ isset($field['inline']) && $field['inline'] ? 'radio-inline' : '' }} form-check-label font-weight-normal">{!! $label !!}</label>
             </div>
-
-            @else
-
-            <div class="form-check">
-                <input  type="radio"
-                		class="form-check-input"
-                		id="{{$field['name']}}_{{$optionPointer}}"
-                		name="{{$field['name']}}"
-                		value="{{$value}}"
-                		@include('crud::fields.inc.attributes')
-                		{{$optionValue == $value ? ' checked': ''}}>
-                <label class="form-check-label font-weight-normal" for="{{$field['name']}}_{{$optionPointer}}">{!! $label !!}</label>
-            </div>
-
-            @endif
 
         @endforeach
 
@@ -60,3 +42,38 @@
     @endif
 
 @include('crud::fields.inc.wrapper_end')
+
+@if ($crud->fieldTypeNotLoaded($field))
+    @php
+        $crud->markFieldTypeAsLoaded($field);
+    @endphp
+
+    {{-- FIELD JS - will be loaded in the after_scripts section --}}
+    @push('crud_fields_scripts')
+    <script>
+        function bpFieldInitRadioElement(element) {
+            var hiddenInput = element.find('input[type=hidden]');
+            var value = hiddenInput.val();
+            var id = 'radio_'+Math.floor(Math.random() * 1000000);
+
+            // set unique IDs so that labels are correlated with inputs
+            element.find('.form-check input[type=radio]').each(function(index, item) {
+                $(this).attr('id', id+index);
+                $(this).siblings('label').attr('for', id+index);
+            });
+
+            // when one radio input is selected
+            element.find('input[type=radio]').change(function(event) {
+                // the value gets updated in the hidden input
+                hiddenInput.val($(this).val());
+                // all other radios get unchecked
+                element.find('input[type=radio]').not(this).prop('checked', false);
+            });
+
+            // check the right items (useful when fields are added using JS - for ex inside the repeatable field)
+            element.find('input[type=radio][value="'+value+'"]').prop('checked', true);
+        }
+    </script>
+    @endpush
+
+@endif

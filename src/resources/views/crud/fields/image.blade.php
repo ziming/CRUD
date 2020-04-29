a941d7939fe3763899509a0cae14b61a8c5e4021
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-04-29 12:23:03 +0300

    addresses #2266 fixes #2709 - makes image and base64_image fields work inside repeatable

--- src/resources/views/crud/fields/image.blade.php
+++ src/resources/views/crud/fields/image.blade.php
@@ -1,7 +1,7 @@
 @php
     $prefix = isset($field['prefix']) ? $field['prefix'] : '';
     $value = old(square_brackets_to_dots($field['name'])) ?? $field['value'] ?? $field['default'] ?? '';
-    $image_url = $value
+    $value = $value
         ? preg_match('/^data\:image\//', $value)
             ? $value
             : (isset($field['disk'])
@@ -51,7 +51,7 @@
     <!-- Wrap the image or canvas element with a block element (container) -->
     <div class="row">
         <div class="col-sm-6" data-handle="previewArea" style="margin-bottom: 20px;">
-            <img data-handle="mainImage" src="{{ $image_url }}">
+            <img data-handle="mainImage" src="">
         </div>
         @if(isset($field['crop']) && $field['crop'])
         <div class="col-sm-3" data-handle="previewArea">
@@ -66,7 +66,7 @@
     <div class="btn-group">
         <div class="btn btn-light btn-sm btn-file">
             {{ trans('backpack::crud.choose_file') }} <input type="file" accept="image/*" data-handle="uploadImage"  @include('crud::fields.inc.attributes', ['default_class' => 'hide'])>
-            <input type="hidden" data-handle="hiddenImage" name="{{ $field['name'] }}">
+            <input type="hidden" data-handle="hiddenImage" name="{{ $field['name'] }}" value="{{ $value }}">
         </div>
         @if(isset($field['crop']) && $field['crop'])
         <button class="btn btn-light btn-sm" data-handle="rotateLeft" type="button" style="display: none;"><i class="la la-rotate-left"></i></button>
@@ -172,12 +172,12 @@
                     var crop = element.attr('data-crop');
 
                     // Hide 'Remove' button if there is no image saved
-                    if (!$mainImage.attr('src')){
+                    if (!$hiddenImage.val()){
                         $previews.hide();
                         $remove.hide();
                     }
-                    // Initialise hidden form input in case we submit with no change
-                    $hiddenImage.val($mainImage.attr('src'));
+                    // Make the main image show the image in the hidden input
+                    $mainImage.attr('src', $hiddenImage.val());
 
 
                     // Only initialize cropper plugin if crop is set to true
@@ -229,10 +229,11 @@
                                 if(crop){
                                     $mainImage.cropper(options).cropper("reset", true).cropper("replace", this.result);
                                     // Override form submit to copy canvas to hidden input before submitting
-                                    $('form').submit(function() {
+                                    // update the hidden input after selecting a new item or cropping
+                                    $mainImage.on('ready cropstart cropend', function() {
                                         var imageURL = $mainImage.cropper('getCroppedCanvas').toDataURL(file.type);
                                         $hiddenImage.val(imageURL);
-                                        return true; // return false to cancel form action
+                                        return true;
                                     });
                                     $rotateLeft.click(function() {
                                         $mainImage.cropper("rotate", 90);

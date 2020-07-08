3615e4b6d804174a7aec584a2bc13659c5afdc9c
Roberto Cestari <robertotcestari@gmail.com>
2020-07-08 10:22:49 -0300

    fix: Bug in image column using a custom disk (s3)

--- src/resources/views/crud/columns/image.blade.php
+++ src/resources/views/crud/columns/image.blade.php
@@ -1,27 +1,29 @@
 {{-- image column type --}}
 @php
   $value = data_get($entry, $column['name']);
+  
+  if($value) {
+    $column['height'] = $column['height'] ?? "25px";
+    $column['width'] = $column['width'] ?? "auto";
+    $column['radius'] = $column['radius'] ?? "3px";
+    $column['prefix'] = $column['prefix'] ?? '';
 
-  $column['height'] = $column['height'] ?? "25px";
-  $column['width'] = $column['width'] ?? "auto";
-  $column['radius'] = $column['radius'] ?? "3px";
-  $column['prefix'] = $column['prefix'] ?? '';
+    if (is_array($value)) {
+      $value = json_encode($value);
+    }
 
-  if (is_array($value)) {
-    $value = json_encode($value);
+    if (preg_match('/^data\:image\//', $value)) { // base64_image
+      $href = $src = $value;
+    } elseif (isset($column['disk'])) { // image from a different disk (like s3 bucket)
+      $href = $src = Storage::disk($column['disk'])->url($column['prefix'].$value);
+    } else { // plain-old image, from a local disk
+      $href = $src = asset( $column['prefix'] . $value);
+    }
+
+    $column['wrapper']['element'] = $column['wrapper']['element'] ?? 'a';
+    $column['wrapper']['href'] = $column['wrapper']['href'] ?? $href;
+    $column['wrapper']['target'] = $column['wrapper']['target'] ?? '_blank';
   }
-
-  if (preg_match('/^data\:image\//', $value)) { // base64_image
-    $href = $src = $value;
-  } elseif (isset($column['disk'])) { // image from a different disk (like s3 bucket)
-    $href = $src = Storage::disk($column['disk'])->url($column['prefix'].$value);
-  } else { // plain-old image, from a local disk
-    $href = $src = asset( $column['prefix'] . $value);
-  }
-
-  $column['wrapper']['element'] = $column['wrapper']['element'] ?? 'a';
-  $column['wrapper']['href'] = $column['wrapper']['href'] ?? $href;
-  $column['wrapper']['target'] = $column['wrapper']['target'] ?? '_blank';
 @endphp
 
 <span>

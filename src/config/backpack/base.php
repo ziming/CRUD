cfda7d95dd62b9b953abca2d22c71e2b71d60a1e
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-10-16 09:29:38 -0400

    default user model is now the one in auth
    Because in Laravel 8+ the default User model has been moved from App\User to App\Models\User, by default when you install Backpack 4.1.x on Laravel 8, you won't be able to login. This is a very bad first experience when installing Backpack, so we should make it "just work" both on Laravel 6 and 7 (App\User) and on Laravel 8 (App\Models\User). One way would be to just use the same config value in the auth.php file - which is what this change does.

--- src/config/backpack/base.php
+++ src/config/backpack/base.php
@@ -207,7 +207,9 @@ return [
     */
 
     // Fully qualified namespace of the User model
-    'user_model_fqn' => App\User::class,
+    'user_model_fqn' => config('auth.providers.users.model'),
+    // 'user_model_fqn' => App\User::class, // works on Laravel <= 7
+    // 'user_model_fqn' => App\Models\User::class, // works on Laravel >= 8
 
     // The classes for the middleware to check if the visitor is an admin
     // Can be a single class or an array of classes

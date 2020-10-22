676ee7698e8c31e68bc8b0f9c338713e8dc20f93
Pedro X <pxpm88@gmail.com>
2020-10-22 13:50:07 +0100

    allow models with arrayed names to be translated.

--- src/resources/views/crud/fields/inc/translatable_icon.blade.php
+++ src/resources/views/crud/fields/inc/translatable_icon.blade.php
@@ -1,3 +1,16 @@
-@if (method_exists($crud->model, 'isTranslatableAttribute') && ($crud->model->isTranslatableAttribute($field['name']) || (isset($field['store_in']) ? $crud->model->isTranslatableAttribute($field['store_in']) : false)) && config('backpack.crud.show_translatable_field_icon'))
+@php
+    //if field name is array we check if any of the arrayed fields is translatable
+    $translatable = false;
+    $translationsEnabled = method_exists($crud->model, 'isTranslatableAttribute');
+    if($translationsEnabled) {
+        foreach((array) $field['name'] as $field_name){
+            if($crud->model->isTranslatableAttribute($field_name)) {
+                $translatable = true;
+            }
+        }
+    }
+
+@endphp
+@if ($translatable || (isset($field['store_in']) ? ($translationsEnabled ? $crud->model->isTranslatableAttribute($field['store_in']) : false) : false) && config('backpack.crud.show_translatable_field_icon'))
 <i class="la la-flag-checkered pull-{{ config('backpack.crud.translatable_field_icon_position') }}" style="margin-top: 3px;" title="This field is translatable."></i>
 @endif

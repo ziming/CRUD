a5c4d6e33ac03d6f38b54ee660a5da8aa3b3b3aa
Pedro X <pxpm88@gmail.com>
2020-11-06 12:43:30 +0000

    refactor dependency selector

--- src/resources/views/crud/fields/relationship/fetch.blade.php
+++ src/resources/views/crud/fields/relationship/fetch.blade.php
@@ -344,19 +344,15 @@
 
             //if element has name it means is not in repeatable, because in repeatable we strip the names out.
             if(typeof element.attr('name') != 'undefined') {
-                $(`[name="${$dependency}"], [name="${$dependency}[]"]`).change(function (el) {
-                    //check if we are dealing with inputs as it's expected and not with some random element in page that happen to have prop `name`
-                    if(el.target.nodeName === 'INPUT' || el.target.nodeName === 'SELECT' || el.target.nodeName === 'TEXTAREA') {
+                form.find(`[name="${$dependency}"], [name="${$dependency}[]"]`).change(function(el) {
                         $(element.find('option:not([value=""])')).remove();
                         element.val(null).trigger("change");
-                    }
                 });
             }else{
                 //this is a repeatable field, we will find the dependency based on row
                 let rowNumber = element.closest('div[data-repeatable-identifier]').attr('data-repeatable-row-number');
 
                 $(`[data-repeatable-input-name="${$dependency}"], [data-repeatable-row-number="${rowNumber}"]`).change(function (el) {
-                    //no need to check element type because `data-repeatable-input-name` is an internal name
                     $(element.find('option:not([value=""])')).remove();
                     element.val(null).trigger("change");
                 });

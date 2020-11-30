591a26fd64df73aa7fe5e70d7e6f7167f0f3f07f
dnwjn <devdnwjn@gmail.com>
2020-11-30 14:04:14 +0100

    Added functionality to allow for callables as wrapper elements

--- src/resources/views/crud/columns/inc/wrapper_end.blade.php
+++ src/resources/views/crud/columns/inc/wrapper_end.blade.php
@@ -1,1 +1,9 @@
-</{{$column['wrapper']['element'] ?? 'a'}}>
+@php
+    // define the wrapper element
+    $wrapperElement = isset($column['wrapper']['element']) ? $column['wrapper']['element'] : 'a';
+    $wrapperElement = !is_string($wrapperElement) && is_callable($wrapperElement)
+        ? $wrapperElement($crud, $column, $entry, $related_key)
+        : $wrapperElement ?? 'a';
+@endphp
+
+</{{$wrapperElement}}>
--- src/resources/views/crud/columns/inc/wrapper_start.blade.php
+++ src/resources/views/crud/columns/inc/wrapper_start.blade.php
@@ -2,6 +2,12 @@
     // this is made available by columns like select and select_multiple
     $related_key = $related_key ?? null;
 
+    // define the wrapper element
+    $wrapperElement = isset($column['wrapper']['element']) ? $column['wrapper']['element'] : 'a';
+    $wrapperElement = !is_string($wrapperElement) && is_callable($wrapperElement)
+        ? $wrapperElement($crud, $column, $entry, $related_key)
+        : $wrapperElement ?? 'a';
+
     // each wrapper attribute can be a callback or a string
     // for those that are callbacks, run the callbacks to get the final string to use
     foreach($column['wrapper'] as $attribute => $value) {
@@ -9,7 +15,7 @@
     }
 @endphp
 
-<{{ $column['wrapper']['element'] ?? 'a' }}
+<{{$wrapperElement}}
 @foreach(Arr::where($column['wrapper'],function($value, $key) { return $key != 'element'; }) as $element => $value)
     {{$element}}="{{$value}}"
 @endforeach

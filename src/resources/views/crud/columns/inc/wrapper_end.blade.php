acca5a8bb04ce00fde94156e75c21c3d6dd67d76
dnwjn <57711725+dnwjn@users.noreply.github.com>
2020-12-01 13:36:19 +0100

    Update src/resources/views/crud/columns/inc/wrapper_end.blade.php
    Co-authored-by: António Almeida <promatik@gmail.com>

--- src/resources/views/crud/columns/inc/wrapper_end.blade.php
+++ src/resources/views/crud/columns/inc/wrapper_end.blade.php
@@ -1,6 +1,6 @@
 @php
     // define the wrapper element
-    $wrapperElement = isset($column['wrapper']['element']) ? $column['wrapper']['element'] : 'a';
+    $wrapperElement = $column['wrapper']['element'] ?? 'a';
     $wrapperElement = !is_string($wrapperElement) && is_callable($wrapperElement)
         ? $wrapperElement($crud, $column, $entry, $related_key)
         : $wrapperElement ?? 'a';

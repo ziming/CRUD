944b32332d9d3b147f0287fc582b03231cd138de
António Almeida <promatik@gmail.com>
2020-12-01 22:01:11 +0000

    Update src/resources/views/crud/columns/inc/wrapper_end.blade.php

--- src/resources/views/crud/columns/inc/wrapper_end.blade.php
+++ src/resources/views/crud/columns/inc/wrapper_end.blade.php
@@ -1,4 +1,7 @@
 @php
+    // this is made available by columns like select and select_multiple
+    $related_key = $related_key ?? null;
+
     // define the wrapper element
     $wrapperElement = $column['wrapper']['element'] ?? 'a';
     $wrapperElement = !is_string($wrapperElement) && is_callable($wrapperElement)

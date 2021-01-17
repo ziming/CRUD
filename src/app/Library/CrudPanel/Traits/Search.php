d39c28a8afa8fe9be6dc0c3e9c9139d70a00fe93
Antonio Almeida <promatik@gmail.com>
2021-01-17 20:14:44 +0000

    Fix for select and select_multiple attribute/name

--- src/app/Library/CrudPanel/Traits/Search.php
+++ src/app/Library/CrudPanel/Traits/Search.php
@@ -66,7 +66,7 @@ trait Search
                 case 'email':
                 case 'text':
                 case 'textarea':
-                    $query->orWhere($this->getColumnWithTableNamePrefixed($query, $column), 'like', '%'.$searchTerm.'%');
+                    $query->orWhere($this->getColumnWithTableNamePrefixed($query, $column['name']), 'like', '%'.$searchTerm.'%');
                     break;
 
                 case 'date':
@@ -77,13 +77,13 @@ trait Search
                         break;
                     }
 
-                    $query->orWhereDate($this->getColumnWithTableNamePrefixed($query, $column), Carbon::parse($searchTerm));
+                    $query->orWhereDate($this->getColumnWithTableNamePrefixed($query, $column['name']), Carbon::parse($searchTerm));
                     break;
 
                 case 'select':
                 case 'select_multiple':
                     $query->orWhereHas($column['entity'], function ($q) use ($column, $searchTerm) {
-                        $q->where($this->getColumnWithTableNamePrefixed($q, $column), 'like', '%'.$searchTerm.'%');
+                        $q->where($this->getColumnWithTableNamePrefixed($q, $column['attribute']), 'like', '%'.$searchTerm.'%');
                     });
                     break;
 
@@ -335,6 +335,6 @@ trait Search
      */
     public function getColumnWithTableNamePrefixed($query, $column)
     {
-        return $query->getModel()->getTableWithPrefix().'.'.$column['attribute'];
+        return $query->getModel()->getTableWithPrefix().'.'.$column;
     }
 }

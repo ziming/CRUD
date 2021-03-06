503ba526067807993611f96eebaefbbfe351e575
Antonio Almeida <promatik@gmail.com>
2021-03-06 19:53:02 +0000

    Fixed default values for upload and upload_multiple

--- src/resources/views/crud/fields/upload_multiple.blade.php
+++ src/resources/views/crud/fields/upload_multiple.blade.php
@@ -1,7 +1,7 @@
 @php
     $field['wrapper'] = $field['wrapper'] ?? $field['wrapperAttributes'] ?? [];
-    $field['wrapper']['data-init-function'] = 'bpFieldInitUploadMultipleElement';
-    $field['wrapper']['data-field-name'] = $field['name'];
+    $field['wrapper']['data-init-function'] = $field['wrapper']['data-init-function'] ?? 'bpFieldInitUploadMultipleElement';
+    $field['wrapper']['data-field-name'] = $field['wrapper']['data-field-name'] ?? $field['name'];
 @endphp
 
 <!-- upload multiple input -->

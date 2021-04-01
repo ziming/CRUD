b28432792add36105d6d139cc579b442ca50c9a1
Pedro X <pxpm88@gmail.com>
2021-04-01 11:02:39 +0100

    check if identifiableAttribute exists in model before calling it

--- src/app/Library/CrudPanel/Traits/FieldsProtectedMethods.php
+++ src/app/Library/CrudPanel/Traits/FieldsProtectedMethods.php
@@ -198,7 +198,9 @@ trait FieldsProtectedMethods
         // if there's a model defined, but no attribute
         // guess an attribute using the indentifiableAttribute functionality in CrudTrait
         if (isset($field['model']) && ! isset($field['attribute'])) {
-            $field['attribute'] = call_user_func([(new $field['model']), 'identifiableAttribute']);
+            if(method_exists($field['model'], 'identifiableAttribute')) {
+                $field['attribute'] = call_user_func([(new $field['model']), 'identifiableAttribute']);
+            }
         }
 
         return $field;

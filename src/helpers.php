21ee4632956865b0441858552ba8b9318f723b1b
Cristian Tabacitu <hello@tabacitu.ro>
2021-09-02 17:07:25 +0300

    change placeholder URL

--- src/helpers.php
+++ src/helpers.php
@@ -99,12 +99,12 @@ if (! function_exists('backpack_avatar_url')) {
     function backpack_avatar_url($user)
     {
         $firstLetter = $user->getAttribute('name') ? mb_substr($user->name, 0, 1, 'UTF-8') : 'A';
-        $placeholder = 'https://placehold.it/160x160/00a65a/ffffff/&text='.$firstLetter;
+        $placeholder = 'https://via.placeholder.com/160x160/00a65a/ffffff/&text='.$firstLetter;
 
         switch (config('backpack.base.avatar_type')) {
             case 'gravatar':
                 if (backpack_users_have_email()) {
-                    return Gravatar::fallback('https://placehold.it/160x160/00a65a/ffffff/&text='.$firstLetter)->get($user->email);
+                    return Gravatar::fallback('https://via.placeholder.com/160x160/00a65a/ffffff/&text='.$firstLetter)->get($user->email);
                 } else {
                     return $placeholder;
                 }

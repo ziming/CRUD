d7a117da49973a24df1fbbea462f8e26abcf0f53
Cristian Tabacitu <hello@tabacitu.ro>
2021-10-08 15:23:29 +0300

    markdown column strips out HTML when escaped

--- src/resources/views/crud/columns/markdown.blade.php
+++ src/resources/views/crud/columns/markdown.blade.php
@@ -1,8 +1,14 @@
 @php
-    $column['text'] = Illuminate\Mail\Markdown::parse($entry->{$column['name']} ?? '');
     $column['escaped'] = $column['escaped'] ?? true;
     $column['prefix'] = $column['prefix'] ?? '';
     $column['suffix'] = $column['suffix'] ?? '';
+    $column['text'] = $entry->{$column['name']} ?? '';
+
+    // if needed, strip out HTML from the text
+    $column['text'] = $column['escaped'] ? strip_tags($column['text']) : $column['text'];
+
+    // turn the text into markdown
+    $column['text'] = Illuminate\Mail\Markdown::parse($column['text']);
 
     if(!empty($column['text'])) {
         $column['text'] = $column['prefix'].$column['text'].$column['suffix'];
@@ -10,10 +16,6 @@
 @endphp
 
 @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_start')
-    @if($column['escaped'])
-        {{ $column['text'] }}
-    @else
-        {!! $column['text'] !!}
-    @endif
+    {!! $column['text'] !!}
 @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_end')
 

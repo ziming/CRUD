2b624a197c65c4bf20916d38851bf611b1b0c73b
Cristian Tabacitu <cristitabacitu@gmail.com>
2021-10-13 08:22:31 +0100

    Update src/resources/lang/en/passwords.php

--- src/resources/lang/en/passwords.php
+++ src/resources/lang/en/passwords.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'throttled' => 'You have requested a password reset recently, please check your email.',
+    'throttled' => 'You have already requested a password reset recently. Please check your email. If you do not receive our email, please retry later.',
     'throttled_request' => 'You have exceeded the limit of tries. Please wait a few minutes and try again.',
 ];

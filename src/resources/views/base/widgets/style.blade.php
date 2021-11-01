c4be075764bdb909a8157cc312e99ed2c6abf629
Cristian Tabacitu <cristitabacitu@gmail.com>
2021-11-01 09:45:49 +0200

    Update style.blade.php

--- src/resources/views/base/widgets/style.blade.php
+++ src/resources/views/base/widgets/style.blade.php
@@ -1,5 +1,12 @@
 @push($widget['stack'] ?? 'after_styles')
 
-    <link href="{{ asset($widget['content'] ?? $widget['path']) }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset($widget['content'] ?? $widget['path']) }}" rel="stylesheet" type="text/css" 
+        @isset($widget['integrity'])
+            integrity="{{ $widget['integrity'] }}"
+        @endisset
+        @isset($widget['crossorigin'])
+            crossorigin="{{ $widget['crossorigin'] }}"
+        @endisset
+        >
 
 @endpush

3d96a468371d5a2d6bdf2eb1ad23d7e3abf90f04
Antonio Almeida <promatik@gmail.com>
2021-11-15 01:53:56 +0000

    Type as "text/css" always

--- src/resources/views/base/widgets/style.blade.php
+++ src/resources/views/base/widgets/style.blade.php
@@ -1,13 +1,12 @@
 @php
     $widget['rel'] = $widget['rel'] ?? 'stylesheet';
-    $widget['type'] = $widget['type'] ?? 'text/css';
 
     $href = asset($widget['href'] ?? $widget['content'] ?? $widget['path']);
     $attributes = collect($widget)->except(['name', 'section', 'type', 'stack', 'href', 'content', 'path'])
 @endphp
 
 @push($widget['stack'] ?? 'after_styles')
-    <link href="{{ $href }}"
+    <link href="{{ $href }}" type="text/css"
         @foreach($attributes as $key => $value)
         {{ $key }}{!! $value === true || $value === '' ? '' : "=\"$value\"" !!}
         @endforeach

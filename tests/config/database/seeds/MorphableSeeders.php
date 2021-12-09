1c92649fe3008e8856c18ff0f7a70432e31ba567
Pedro X <pxpm88@gmail.com>
2021-12-09 15:30:34 +0000

    add tests, refactor pivot relations

---
+++ tests/config/database/seeds/MorphableSeeders.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Backpack\CRUD\Tests\Config\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class MorphableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        $now = \Carbon\Carbon::now();
+
+        DB::table('recommends')->insert([[
+            'title' => $faker->title,
+            'created_at'     => $now,
+            'updated_at'     => $now,
+        ],[
+            'title' => $faker->title,
+            'created_at'     => $now,
+            'updated_at'     => $now,
+        ]]);
+
+        DB::table('bills')->insert([[
+            'title' => $faker->title,
+            'created_at'     => $now,
+            'updated_at'     => $now,
+        ],[
+            'title' => $faker->title,
+            'created_at'     => $now,
+            'updated_at'     => $now,
+        ]]);
+    }
+}

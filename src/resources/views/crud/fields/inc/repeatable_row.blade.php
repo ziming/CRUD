11cc1887b328423cd9f207a21c46c6658c873443
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2021-12-21 17:48:17 +0200

    added "reorder" attribute to repeatable field

--- src/resources/views/crud/fields/inc/repeatable_row.blade.php
+++ src/resources/views/crud/fields/inc/repeatable_row.blade.php
@@ -7,12 +7,14 @@
     @if (isset($field['fields']) && is_array($field['fields']) && count($field['fields']))
     <div class="controls">
         <button type="button" class="close delete-element"><span aria-hidden="true">×</span></button>
+        @if ($field['reorder'])
         <button type="button" class="close move-element-up">
             <svg viewBox="0 0 64 80"><path d="M46.8,36.7c-4.3-4.3-8.7-8.7-13-13c-1-1-2.6-1-3.5,0c-4.3,4.3-8.7,8.7-13,13c-2.3,2.3,1.3,5.8,3.5,3.5c4.3-4.3,8.7-8.7,13-13c-1.2,0-2.4,0-3.5,0c4.3,4.3,8.7,8.7,13,13C45.5,42.5,49,39,46.8,36.7L46.8,36.7z"/></svg>
         </button>
         <button type="button" class="close move-element-down">
             <svg viewBox="0 0 64 80"><path d="M17.2,30.3c4.3,4.3,8.7,8.7,13,13c1,1,2.6,1,3.5,0c4.3-4.3,8.7-8.7,13-13c2.3-2.3-1.3-5.8-3.5-3.5c-4.3,4.3-8.7,8.7-13,13c1.2,0,2.4,0,3.5,0c-4.3-4.3-8.7-8.7-13-13C18.5,24.5,15,28,17.2,30.3L17.2,30.3z"/></svg>
         </button>
+        @endif
     </div>
     @foreach($field['fields'] as $subfield)
         @php

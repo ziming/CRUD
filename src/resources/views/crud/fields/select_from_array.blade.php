29c8f8ebb8f05bd66941828862d43c7314a07c31
Pedro X <pxpm88@gmail.com>
2021-12-28 12:14:26 +0000

    refactor select from array

--- src/resources/views/crud/fields/select_from_array.blade.php
+++ src/resources/views/crud/fields/select_from_array.blade.php
@@ -1,5 +1,6 @@
 @php
     $field['allows_null'] = $field['allows_null'] ?? $crud->model::isColumnNullable($field['name']);
+    $field['value'] = old_empty_or_null($field['name'], '') ??  $field['value'] ?? $field['default'] ?? '';
 @endphp
 <!-- select from array -->
 @include('crud::fields.inc.wrapper_start')
@@ -17,25 +18,7 @@
 
         @if (count($field['options']))
             @foreach ($field['options'] as $key => $value)
-                @if((old(square_brackets_to_dots($field['name'])) !== null && (
-                        $key == old(square_brackets_to_dots($field['name'])) ||
-                        (is_array(old(square_brackets_to_dots($field['name']))) &&
-                        in_array($key, old(square_brackets_to_dots($field['name'])))))) ||
-                        (null === old(square_brackets_to_dots($field['name'])) &&
-                            ((isset($field['value']) && (
-                                        $key == $field['value'] || (
-                                                is_array($field['value']) &&
-                                                in_array($key, $field['value'])
-                                                )
-                                        )) ||
-                                (!isset($field['value']) && isset($field['default']) &&
-                                ($key == $field['default'] || (
-                                                is_array($field['default']) &&
-                                                in_array($key, $field['default'])
-                                            )
-                                        )
-                                ))
-                        ))
+                @if($key == $field['value'] || (is_array($field['value']) && in_array($key, $field['value'])))
                     <option value="{{ $key }}" selected>{{ $value }}</option>
                 @else
                     <option value="{{ $key }}">{{ $value }}</option>

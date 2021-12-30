ea95f912a6dfe1b8c5eb86e4ff7b79987c820afd
Pedro X <pxpm88@gmail.com>
2021-12-30 10:43:38 +0000

    refactor select2_from_array

--- src/resources/views/crud/fields/select2_from_array.blade.php
+++ src/resources/views/crud/fields/select2_from_array.blade.php
@@ -1,6 +1,7 @@
 <!-- select2 from array -->
 @php
     $field['allows_null'] = $field['allows_null'] ?? $crud->model::isColumnNullable($field['name']);
+    $field['value'] = old_empty_or_null($field['name'], '') ??  $field['value'] ?? $field['default'] ?? '';
 @endphp
 @include('crud::fields.inc.wrapper_start')
     <label>{!! $field['label'] !!}</label>
@@ -20,25 +21,7 @@
 
         @if (count($field['options']))
             @foreach ($field['options'] as $key => $value)
-                @if((old(square_brackets_to_dots($field['name'])) && (
-                        $key == old(square_brackets_to_dots($field['name'])) ||
-                        (is_array(old(square_brackets_to_dots($field['name']))) &&
-                        in_array($key, old(square_brackets_to_dots($field['name'])))))) ||
-                        (null === old(square_brackets_to_dots($field['name'])) &&
-                            ((isset($field['value']) && (
-                                        $key == $field['value'] || (
-                                                is_array($field['value']) &&
-                                                in_array($key, $field['value'])
-                                                )
-                                        )) ||
-                                (!isset($field['value']) && isset($field['default']) &&
-                                ($key == $field['default'] || (
-                                                is_array($field['default']) &&
-                                                in_array($key, $field['default'])
-                                            )
-                                        )
-                                ))
-                        ))
+                @if($key == $field['value'] || (is_array($field['value']) && in_array($key, $field['value'])))
                     <option value="{{ $key }}" selected>{{ $value }}</option>
                 @else
                     <option value="{{ $key }}">{{ $value }}</option>

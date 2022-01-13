d537da01d09aa044e8f290a4b1e932c51c425fa0
Pedro X <pxpm88@gmail.com>
2022-01-13 13:08:52 +0000

    menually configure the pivot field

--- src/resources/views/crud/fields/relationship/entries.blade.php
+++ src/resources/views/crud/fields/relationship/entries.blade.php
@@ -8,21 +8,27 @@
 
 @php
     $field['type'] = 'repeatable';
-    //each row represent a related entry in a database table. We should not "auto-add" one relationship if it's not the user intention.
-    $field['init_rows'] = 0;
     $field['fields'] = $field['pivotFields'];
     $field['reorder'] = $field['reorder'] ?? false;
-    $inline_create = !isset($inlineCreate) && isset($pivotSelectorField['inline_create']) ? $pivotSelectorField['inline_create'] : false;
     $pivotSelectorField = $field['pivotSelect'] ?? [];
+    $inline_create = !isset($inlineCreate) && isset($pivotSelectorField['inline_create']) ? $pivotSelectorField['inline_create'] : false;
     $pivotSelectorField['name'] = $field['name'];
     $pivotSelectorField['is_pivot_select'] = true;
     $pivotSelectorField['multiple'] = false;
+    $pivotSelectorField['allows_null'] = false;
+    $pivotSelectorField['entity'] = $field['entity'];
+    $pivotSelectorField['model'] = $field['model'];
+    
     $pivotSelectorField['ajax'] = $pivotSelectorField['ajax'] ?? false;
     $pivotSelectorField['data_source'] = $pivotSelectorField['data_source'] ?? isset($pivotSelectorField['ajax']) && $pivotSelectorField['ajax'] ? url($crud->route.'/fetch/'.$field['entity']) : 'false';
     $pivotSelectorField['minimum_input_length'] = $pivotSelectorField['minimum_input_length'] ?? 2;
     $pivotSelectorField['delay'] = $pivotSelectorField['delay'] ?? 500;
     $pivotSelectorField['placeholder'] = $pivotSelectorField['placeholder'] ?? trans('backpack::crud.select_entry');
-
+    $pivotSelectorField['type'] = $inline_create !== false ? 'fetch_or_create' : ($pivotSelectorField['ajax'] ? 'fetch' : 'select');
+    $pivotSelectorField['view_namespace'] = 'crud::fields.relationship';
+    $pivotSelectorField['attribute'] = $pivotSelectorField['attribute'] ?? (new $field['model'])->identifiableAttribute();
+    
+    
     if($inline_create) {
         $field['inline_create'] = $inline_create;
     }

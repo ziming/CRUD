7b03b9fd8d56c8fbf651a4e36efac31ccada4be7
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2022-01-24 08:44:46 +0200

    moved chart widget to PRO

--- src/resources/views/base/inc/widgets.blade.php
+++ src/resources/views/base/inc/widgets.blade.php
@@ -2,10 +2,12 @@
 	@foreach ($widgets as $currentWidget)
 
 		@if (is_array($currentWidget))
-			@include($currentWidget['viewNamespace'] ?? config('backpack.base.component_view_namespaces.widgets')[0] .'.'. $currentWidget['type'], ['widget' => $currentWidget])
-		@else
-			@include($currentWidget->getFinalViewPath(), ['widget' => $currentWidget->toArray()])
+			@php
+				$currentWidget = \Backpack\CRUD\app\Library\Widget::add($currentWidget);
+			@endphp
 		@endif
+
+		@include($currentWidget->getFinalViewPath(), ['widget' => $currentWidget->toArray()])
 
 	@endforeach
 @endif

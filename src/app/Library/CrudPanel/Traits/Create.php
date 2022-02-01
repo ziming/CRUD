d06059de9c009158d590767c3c74e6bdbebfe046
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2022-02-01 10:56:01 +0200

    change order of methods in Create trait

--- src/app/Library/CrudPanel/Traits/Create.php
+++ src/app/Library/CrudPanel/Traits/Create.php
@@ -14,6 +14,16 @@ trait Create
     */
 
     /**
+     * Get all fields needed for the ADD NEW ENTRY form.
+     *
+     * @return array The fields with attributes and fake attributes.
+     */
+    public function getCreateFields()
+    {
+        return $this->fields();
+    }
+
+    /**
      * Insert a row in the database.
      *
      * @param  array  $input  All input values to be inserted.
@@ -26,6 +36,31 @@ trait Create
         $this->createRelationsForItem($item, $relationInputs);
 
         return $item;
+    }
+
+    /**
+     * ---------------
+     * PRIVATE METHODS
+     * ---------------
+     */
+
+    /**
+     * Returns the direct inputs parsed for model and relationship creation.
+     *
+     * @param  array  $inputs
+     * @param  null|array  $relationDetails
+     * @param  bool|string  $relationMethod
+     * @return array
+     */
+    private function splitInputIntoDirectAndRelations($inputs, $relationDetails = null, $relationMethod = false)
+    {
+        $crudFields = $relationDetails['crudFields'] ?? [];
+        $model = $relationDetails['model'] ?? false;
+
+        $directInputs = $this->getDirectInputsFromInput($inputs, $model, $crudFields, $relationMethod);
+        $relationInputs = $this->getRelationDetailsFromInput($inputs, $crudFields, $relationMethod);
+
+        return [$directInputs, $relationInputs];
     }
 
     /**
@@ -45,11 +80,110 @@ trait Create
 
         $input = $this->decodeJsonCastedAttributes($input, $model);
         $input = $this->compactFakeFields($input);
-
         $input = $this->excludeRelationFieldsExceptBelongsTo($input, $fields, $relationMethod);
         $input = $this->changeBelongsToNamesFromRelationshipToForeignKey($input, $fields);
 
         return $input;
+    }
+
+    /**
+     * Get a relation data array from the form data. For each relation defined in the fields
+     * through the entity attribute, and set some relation details.
+     *
+     * We traverse this relation array later to create the relations, for example:
+     * - Current model HasOne Address
+     * - Address (line_1, country_id) BelongsTo Country through country_id in Address Model
+     *
+     * So when editing current model crud user have two fields
+     * - address.line_1
+     * - address.country
+     * (we infer country_id from relation)
+     *
+     * Those will be nested accordingly in this relation array, so address relation
+     * will have a nested relation with country.
+     *
+     * @param  array  $input  The form input.
+     * @param  array  $crudFields  - present when getting the relation details for other relations.
+     * @param  mixed  $relationMethod
+     * @return array The formatted relation details.
+     */
+    private function getRelationDetailsFromInput($input, $crudFields = [], $relationMethod = false)
+    {
+        // main entity
+        if (empty($crudFields)) {
+            $relationFields = $this->getRelationFields();
+        } else {
+            // relations sends the fields that represent them so we can parse the input accordingly.
+            $relationFields = $crudFields;
+
+            foreach ($crudFields as $key => $crudField) {
+                if (isset($crudField['subfields'])) {
+                    foreach ($crudField['subfields'] as $crudSubField) {
+                        if (isset($crudSubField['relation_type'])) {
+                            $relationFields[] = $crudSubField;
+                        }
+                    }
+                }
+            }
+        }
+
+        //remove fields that are not in the submitted form input
+        $relationFields = array_filter($relationFields, function ($field) use ($input) {
+            return Arr::has($input, $field['name']) || isset($input[$field['name']]) || Arr::has($input, Str::afterLast($field['name'], '.'));
+        });
+
+        $relationDetails = [];
+
+        foreach ($relationFields as $field) {
+            // if relationMethod is set we strip it out of the fieldName that we use to create the relations array
+            $fieldName = $relationMethod ? Str::after($field['name'], $relationMethod.'.') : $field['name'];
+
+            $key = Str::before($this->getOnlyRelationEntity(['entity' => $fieldName]), '.');
+
+            // if the field entity contains the attribute we want to add that attribute in the correct relation key.
+            // eg: adress.street, we want to add `street` as an attribute in `address` relation, `street` is not
+            // a relation of `address`
+            if ($this->getOnlyRelationEntity($field) !== $field['entity']) {
+                if (Str::before($field['entity'], '.') === $relationMethod) {
+                    $key = Str::before($this->getOnlyRelationEntity($field), '.');
+                }
+            }
+
+            $attributeName = (string) Str::of($field['name'])->afterLast('.');
+
+            switch ($field['relation_type']) {
+                case 'BelongsTo':
+                    // when it's a nested belongsTo relation we want to make sure
+                    // the key used to store the values is the main relation key
+                    $key = Str::beforeLast($this->getOnlyRelationEntity($field), '.');
+
+                break;
+            }
+
+            // we don't need to re-setup this relation method values, we just want the relations
+            if ($key === $relationMethod) {
+                continue;
+            }
+
+            $fieldDetails = Arr::get($relationDetails, $key, []);
+
+            $fieldDetails['values'][$attributeName] = Arr::get($input, $fieldName);
+            $fieldDetails['model'] = $fieldDetails['model'] ?? $field['model'];
+            $fieldDetails['relation_type'] = $fieldDetails['relation_type'] ?? $field['relation_type'];
+            $fieldDetails['crudFields'][] = $field;
+            $fieldDetails['relationEntity'] = $this->getOnlyRelationEntity($field);
+
+            if (isset($field['fallback_id'])) {
+                $fieldDetails['fallback_id'] = $field['fallback_id'];
+            }
+            if (isset($field['force_delete'])) {
+                $fieldDetails['force_delete'] = $field['force_delete'];
+            }
+
+            Arr::set($relationDetails, $key, $fieldDetails);
+        }
+
+        return $relationDetails;
     }
 
     /**
@@ -90,15 +224,6 @@ trait Create
         });
     }
 
-    /**
-     * Get all fields needed for the ADD NEW ENTRY form.
-     *
-     * @return array The fields with attributes and fake attributes.
-     */
-    public function getCreateFields()
-    {
-        return $this->fields();
-    }
 
     /**
      * Get all fields with relation set (model key set on field).
@@ -258,25 +383,6 @@ trait Create
     }
 
     /**
-     * Returns the direct inputs parsed for model and relationship creation.
-     *
-     * @param  array  $inputs
-     * @param  null|array  $relationDetails
-     * @param  bool|string  $relationMethod
-     * @return array
-     */
-    private function splitInputIntoDirectAndRelations($inputs, $relationDetails = null, $relationMethod = false)
-    {
-        $crudFields = $relationDetails['crudFields'] ?? [];
-        $model = $relationDetails['model'] ?? false;
-
-        $directInputs = $this->getDirectInputsFromInput($inputs, $model, $crudFields, $relationMethod);
-        $relationInputs = $this->getRelationDetailsFromInput($inputs, $crudFields, $relationMethod);
-
-        return [$directInputs, $relationInputs];
-    }
-
-    /**
      * When using the HasMany/MorphMany relations as selectable elements we use this function to "mimic-sync" in those relations.
      * Since HasMany/MorphMany does not have the `sync` method, we manually re-create it.
      * Here we add the entries that developer added and remove the ones that are not in the list.
@@ -388,105 +494,5 @@ trait Create
             // we perform the cleanup of removed database items
             $entry->{$relationMethod}()->whereNotIn($relation_local_key, $relatedItemsSent)->delete();
         }
-    }
-
-    /**
-     * Get a relation data array from the form data. For each relation defined in the fields
-     * through the entity attribute, and set some relation details.
-     *
-     * We traverse this relation array later to create the relations, for example:
-     * - Current model HasOne Address
-     * - Address (line_1, country_id) BelongsTo Country through country_id in Address Model
-     *
-     * So when editing current model crud user have two fields
-     * - address.line_1
-     * - address.country
-     * (we infer country_id from relation)
-     *
-     * Those will be nested accordingly in this relation array, so address relation
-     * will have a nested relation with country.
-     *
-     * @param  array  $input  The form input.
-     * @param  array  $crudFields  - present when getting the relation details for other relations.
-     * @param  mixed  $relationMethod
-     * @return array The formatted relation details.
-     */
-    private function getRelationDetailsFromInput($input, $crudFields = [], $relationMethod = false)
-    {
-        // main entity
-        if (empty($crudFields)) {
-            $relationFields = $this->getRelationFields();
-        } else {
-            // relations sends the fields that represent them so we can parse the input accordingly.
-            $relationFields = $crudFields;
-
-            foreach ($crudFields as $key => $crudField) {
-                if (isset($crudField['subfields'])) {
-                    foreach ($crudField['subfields'] as $crudSubField) {
-                        if (isset($crudSubField['relation_type'])) {
-                            $relationFields[] = $crudSubField;
-                        }
-                    }
-                }
-            }
-        }
-
-        //remove fields that are not in the submitted form input
-        $relationFields = array_filter($relationFields, function ($field) use ($input) {
-            return Arr::has($input, $field['name']) || isset($input[$field['name']]) || Arr::has($input, Str::afterLast($field['name'], '.'));
-        });
-
-        $relationDetails = [];
-
-        foreach ($relationFields as $field) {
-            // if relationMethod is set we strip it out of the fieldName that we use to create the relations array
-            $fieldName = $relationMethod ? Str::after($field['name'], $relationMethod.'.') : $field['name'];
-
-            $key = Str::before($this->getOnlyRelationEntity(['entity' => $fieldName]), '.');
-
-            // if the field entity contains the attribute we want to add that attribute in the correct relation key.
-            // eg: adress.street, we want to add `street` as an attribute in `address` relation, `street` is not
-            // a relation of `address`
-            if ($this->getOnlyRelationEntity($field) !== $field['entity']) {
-                if (Str::before($field['entity'], '.') === $relationMethod) {
-                    $key = Str::before($this->getOnlyRelationEntity($field), '.');
-                }
-            }
-
-            $attributeName = (string) Str::of($field['name'])->afterLast('.');
-
-            switch ($field['relation_type']) {
-                case 'BelongsTo':
-                    // when it's a nested belongsTo relation we want to make sure
-                    // the key used to store the values is the main relation key
-                    $key = Str::beforeLast($this->getOnlyRelationEntity($field), '.');
-
-                break;
-            }
-
-            // we don't need to re-setup this relation method values, we just want the relations
-            if ($key === $relationMethod) {
-                continue;
-            }
-
-            $fieldDetails = Arr::get($relationDetails, $key, []);
-
-            $fieldDetails['values'][$attributeName] = Arr::get($input, $fieldName);
-            $fieldDetails['model'] = $fieldDetails['model'] ?? $field['model'];
-            $fieldDetails['relation_type'] = $fieldDetails['relation_type'] ?? $field['relation_type'];
-            $fieldDetails['crudFields'][] = $field;
-            $fieldDetails['relationEntity'] = $this->getOnlyRelationEntity($field);
-
-            if (isset($field['fallback_id'])) {
-                $fieldDetails['fallback_id'] = $field['fallback_id'];
-            }
-            if (isset($field['force_delete'])) {
-                $fieldDetails['force_delete'] = $field['force_delete'];
-            }
-
-            Arr::set($relationDetails, $key, $fieldDetails);
-        }
-
-        return $relationDetails;
     }
 }

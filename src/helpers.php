177a47ec833efbf65631e0069902507419d16bad
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2022-02-08 08:36:38 +0200

    do not allow manually-installed backpack/pro

--- src/helpers.php
+++ src/helpers.php
@@ -306,10 +306,6 @@ if (! function_exists('backpack_pro')) {
      */
     function backpack_pro()
     {
-        if (array_key_exists('Backpack\Pro\AddonServiceProvider', app()->getLoadedProviders())) {
-            return true;
-        }
-
         if (! \Composer\InstalledVersions::isInstalled('backpack/pro')) {
             return false;
         }

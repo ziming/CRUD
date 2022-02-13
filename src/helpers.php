cab1183e44010627ec22dc0ee6bfadd0a996da9f
Antonio Almeida <promatik@gmail.com>
2022-02-13 19:33:01 +0000

    Removed is_countable helper

--- src/helpers.php
+++ src/helpers.php
@@ -232,22 +232,7 @@ if (! function_exists('square_brackets_to_dots')) {
     }
 }
 
-if (! function_exists('is_countable')) {
-    /**
-     * We need this because is_countable was only introduced in PHP 7.3,
-     * and in PHP 7.2 you should check if count() argument is really countable.
-     * This function may be removed in future if PHP >= 7.3 becomes a requirement.
-     *
-     * @param $obj
-     * @return bool
-     */
-    function is_countable($obj)
-    {
-        return is_array($obj) || $obj instanceof Countable;
-    }
-}
-
-if (! function_exists('old_empty_or_null')) {
+if (!function_exists('old_empty_or_null')) {
     /**
      * This method is an alternative to Laravel's old() helper, which mistakenly
      * returns NULL it two cases:

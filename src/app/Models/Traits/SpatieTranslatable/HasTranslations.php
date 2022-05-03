ae1525e37dbc6b4c97d9f8f7611fa1ed57a113d7
Mikhail <supermen696@yandex.ru>
2022-05-03 16:20:20 +0400

    Update HasTranslations.php

--- src/app/Models/Traits/SpatieTranslatable/HasTranslations.php
+++ src/app/Models/Traits/SpatieTranslatable/HasTranslations.php
@@ -187,7 +187,6 @@ trait HasTranslations
             case 'findMany':
             case 'findBySlug':
             case 'findBySlugOrFail':
-            case 'hydrate':
                 $translation_locale = \Request::input('_locale', \App::getLocale());
 
                 if ($translation_locale) {

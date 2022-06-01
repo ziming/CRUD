3228170bf6f71ad977a68feb98e6e7e475beac6b
Lennert Daniels <lennert.daniels@gmail.com>
2022-06-01 13:39:04 +0200

    added field prefix & suffix where possible

--- src/resources/views/crud/fields/color.blade.php
+++ src/resources/views/crud/fields/color.blade.php
@@ -2,12 +2,17 @@
 @include('crud::fields.inc.wrapper_start')
     <label>{!! $field['label'] !!}</label>
     @include('crud::fields.inc.translatable_icon')
-    <input
-    	type="color"
-    	name="{{ $field['name'] }}"
-        value="{{ old_empty_or_null($field['name'], '') ??  $field['value'] ?? $field['default'] ?? '' }}"
-        @include('crud::fields.inc.attributes')
+
+    @if(isset($field['prefix']) || isset($field['suffix'])) <div class="input-group"> @endif
+        @if(isset($field['prefix'])) <div class="input-group-prepend"><span class="input-group-text">{!! $field['prefix'] !!}</span></div> @endif
+        <input
+            type="color"
+            name="{{ $field['name'] }}"
+            value="{{ old_empty_or_null($field['name'], '') ??  $field['value'] ?? $field['default'] ?? '' }}"
+            @include('crud::fields.inc.attributes')
     	>
+        @if(isset($field['suffix'])) <div class="input-group-append"><span class="input-group-text">{!! $field['suffix'] !!}</span></div> @endif
+    @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
 
     {{-- HINT --}}
     @if (isset($field['hint']))
--- src/resources/views/crud/fields/datetime.blade.php
+++ src/resources/views/crud/fields/datetime.blade.php
@@ -15,13 +15,18 @@ $value = $timestamp ? strftime('%Y-%m-%dT%H:%M:%S', $timestamp) : '';
 @include('crud::fields.inc.wrapper_start')
     <label>{!! $field['label'] !!}</label>
     @include('crud::fields.inc.translatable_icon')
-    <input
-        type="datetime-local"
-        name="{{ $field['name'] }}"
-        value="{{ $value }}"
-        @include('crud::fields.inc.attributes')
-        >
 
+    @if(isset($field['prefix']) || isset($field['suffix'])) <div class="input-group"> @endif
+        @if(isset($field['prefix'])) <div class="input-group-prepend"><span class="input-group-text">{!! $field['prefix'] !!}</span></div> @endif
+        <input
+            type="datetime-local"
+            name="{{ $field['name'] }}"
+            value="{{ $value }}"
+            @include('crud::fields.inc.attributes')
+        >
+        @if(isset($field['suffix'])) <div class="input-group-append"><span class="input-group-text">{!! $field['suffix'] !!}</span></div> @endif
+    @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
+    
     {{-- HINT --}}
     @if (isset($field['hint']))
         <p class="help-block">{!! $field['hint'] !!}</p>

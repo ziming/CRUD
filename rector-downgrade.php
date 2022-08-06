dea87ac5f962b2823bb143663503a131bf586a74
ziming <ziming@users.noreply.github.com>
2022-08-06 11:07:28 +0800

    Rector PHP Downgrade Dry Run Github Action

---
+++ rector-downgrade.php
@@ -0,0 +1,16 @@
+<?php
+
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->sets([
+        DowngradeLevelSetList::DOWN_TO_PHP_73
+    ]);
+};

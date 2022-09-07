cf5fc88fc016b596b95760c01b0b0dcb15d85fd2
Pedro X <pxpm88@gmail.com>
2022-09-07 10:29:01 +0100

    add shame not

--- src/app/Library/CrudPanel/Traits/HasViewNamespaces.php
+++ src/app/Library/CrudPanel/Traits/HasViewNamespaces.php
@@ -4,8 +4,19 @@ namespace Backpack\CRUD\app\Library\CrudPanel\Traits;
 
 use Backpack\CRUD\ViewNamespaces;
 
+
+
 trait HasViewNamespaces
 {
+    /**
+     * This file is only needed because we messed up version constrains from 1.2 up to 1.2.6 of PRO version
+     * and any user that the license ended in the middle of those versions was not able to update
+     * Backpack/CRUD up from 5.3.6.
+     * 
+     * This should be removed in the next major version.
+     *
+     */
+
     public function addViewNamespacesFor(string $domain, array $viewNamespaces)
     {
         ViewNamespaces::addFor($domain, $viewNamespaces);

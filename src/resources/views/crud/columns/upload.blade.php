535938830c8da906927c15ac1ba42ecae5a1da11
Munjal Mayank <munjaldevelopment@gmail.com>
2022-10-03 12:41:06 +0530

    disk code added
    Disk href added along with wrapper & href

--- src/resources/views/crud/columns/upload.blade.php
+++ src/resources/views/crud/columns/upload.blade.php
@@ -3,6 +3,8 @@
     $column['value'] = $column['value'] ?? data_get($entry, $column['name']);
     $column['wrapper']['element'] = $column['wrapper']['element'] ?? 'a';
     $column['wrapper']['target'] = $column['wrapper']['target'] ?? '_blank';
-    $column['wrapper']['href'] = asset('/').$column['value'];
+    $column_wrapper_href = $column['wrapper']['href'] ?? function($file_path, $disk) { return ( !is_null($disk) ?asset(\Storage::disk($disk)->url($file_path)):asset($file_path) ); };
+
+    $column['wrapper']['href'] = $column_wrapper_href instanceof \Closure ? $column_wrapper_href($column['value'], $column['disk']) : $column_wrapper_href;
 @endphp
 @include('crud::columns.text')

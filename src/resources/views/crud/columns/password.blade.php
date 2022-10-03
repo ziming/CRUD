e3bb1adf9a09d4cbab073caa0d35189201f63f3b
Munjal Mayank <munjaldevelopment@gmail.com>
2022-10-03 17:19:14 +0530

    fixed value to '******'
    fixed value to '******' as discussed

--- src/resources/views/crud/columns/password.blade.php
+++ src/resources/views/crud/columns/password.blade.php
@@ -1,17 +1,6 @@
 @php
-    $column['value'] = $column['value'] ?? data_get($entry, $column['name']);
-    $column['value'] = str_repeat("*", strlen($column['value']));
+    $column['value'] = "******";
     $column['escaped'] = $column['escaped'] ?? true;
     $column['limit'] = $column['limit'] ?? 6;
-
-    if($column['value'] instanceof \Closure) {
-        $column['value'] = $column['value']($entry);
-    }
-
-    if(!empty($column['value'])) {
-        $column['text'] = Str::limit($column['value'], $column['limit'], '');
-    }
 @endphp
-<span>
-    {{ $column['text'] }}
-</span>
+@include('crud::columns.text')

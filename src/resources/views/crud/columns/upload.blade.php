f0beb2914e7423a8847c66c3a784613dc99176e8
Munjal Mayank <munjaldevelopment@gmail.com>
2022-10-17 15:22:37 +0530

    remove "download" attribute
    removed "download" attribute as that's NOT needed..

--- src/resources/views/crud/columns/upload.blade.php
+++ src/resources/views/crud/columns/upload.blade.php
@@ -3,7 +3,6 @@
     $column['value'] = $column['value'] ?? data_get($entry, $column['name']);
     $column['wrapper']['element'] = $column['wrapper']['element'] ?? 'a';
     $column['wrapper']['target'] = $column['wrapper']['target'] ?? '_blank';
-    $column['wrapper']['download'] = $column['wrapper']['download'] ?? 'download';
     $column_wrapper_href = $column['wrapper']['href'] ?? function($file_path) { return asset($file_path); };
 
     $column['wrapper']['href'] = $column_wrapper_href instanceof \Closure ? $column_wrapper_href($column['value'], $column['disk']) : $column_wrapper_href;

70b3403628743ba84de0e5d7a3e9e86a634106c4
Munjal Mayank <munjaldevelopment@gmail.com>
2022-10-17 15:21:36 +0530

    disk section removed from wrapper href
    disk section removed from wrapper href & calling "field_name" directly

--- src/resources/views/crud/columns/upload.blade.php
+++ src/resources/views/crud/columns/upload.blade.php
@@ -4,7 +4,7 @@
     $column['wrapper']['element'] = $column['wrapper']['element'] ?? 'a';
     $column['wrapper']['target'] = $column['wrapper']['target'] ?? '_blank';
     $column['wrapper']['download'] = $column['wrapper']['download'] ?? 'download';
-    $column_wrapper_href = $column['wrapper']['href'] ?? function($file_path, $disk) { return ( !is_null($disk) ?asset(\Storage::disk($disk)->url($file_path)):asset($file_path) ); };
+    $column_wrapper_href = $column['wrapper']['href'] ?? function($file_path) { return asset($file_path); };
 
     $column['wrapper']['href'] = $column_wrapper_href instanceof \Closure ? $column_wrapper_href($column['value'], $column['disk']) : $column_wrapper_href;
 @endphp

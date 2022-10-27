3e5fb7603f437533e6a666abc22fd5ebd13650eb
Munjal Mayank <munjaldevelopment@gmail.com>
2022-10-27 13:05:58 +0530

    color column UI changes
    color column UI changes implemented

--- src/resources/views/crud/columns/color.blade.php
+++ src/resources/views/crud/columns/color.blade.php
@@ -17,7 +17,9 @@
     @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_start')
         @if($column['escaped'])
             @if($column['text'] != "-")
-                <span title="{{ $column['text'] }}" class="btn rounded-circle" style="font-size: 0.5rem; background-color: {{ $column['text'] }}">&nbsp;</span><br />
+                <span title="{{ $column['text'] }}" class="btn rounded-circle" style="font-size: 0.5rem; background-color: {{ $column['text'] }}">&nbsp;</span>
+            @else
+                {{ $column['text'] }}
             @endif
         @else
             {!! $column['text'] !!} &nbsp;<span title="{{ $column['text'] }}" class="btn" style="width: 16px; height: 16px; padding: 0px; border-radius: 0px; background-color: {{ $column['text'] }}">&nbsp;</span>

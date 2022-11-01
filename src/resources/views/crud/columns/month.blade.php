c7ba1ecdc4c56022f5aef47b843d61b03ce0ed5c
Cristian Tăbăcitu <cristitabacitu@gmail.com>
2022-11-01 11:32:23 +0200

    Apply suggestions from code review

--- src/resources/views/crud/columns/month.blade.php
+++ src/resources/views/crud/columns/month.blade.php
@@ -1,4 +1,4 @@
 @php
-    $column['format'] = $column['format'] ?? 'MMMM-Y';
+    $column['format'] = $column['format'] ?? 'MMMM Y';
 @endphp
 @include('crud::columns.date')
--- src/resources/views/crud/columns/week.blade.php
+++ src/resources/views/crud/columns/week.blade.php
@@ -1,5 +1,6 @@
 @php
     $column['prefix'] = $column['prefix'] ?? 'Week ';
-    $column['format'] = $column['format'] ?? 'W-Y';
+    $column['format'] = $column['format'] ?? 'W Y';
+
 @endphp
 @include('crud::columns.date')

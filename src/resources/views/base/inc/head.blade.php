438f934574832f7e426ddd324db1b327a2b2adff
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-01-04 11:28:24 +0200

    completely remove all JS and CSS assets from this package, along with NPM

--- src/resources/views/base/inc/head.blade.php
+++ src/resources/views/base/inc/head.blade.php
@@ -10,14 +10,14 @@
     @stack('before_styles')
 
     @basset('https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css')
-    @basset('packages/backpack/base/css/bundle.css')
+    @basset('https://unpkg.com/@digitallyhappy/backstrap@0.5.1/dist/css/legacy.css')
     @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css')
     @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/fonts/la-regular-400.woff2')
     @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/fonts/la-solid-900.woff2')
     @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/fonts/la-brands-400.woff2')
 
-    @basset('packages/source-sans-pro/source-sans-pro.css')
-    {{-- The above should be turned into the following, but it doesn't quite work yet: --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
+    {{-- TODO: We should use Basset for Google Fonts too, but it doesn't quite work yet: --}}
     {{-- @basset('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap', true, [], 'style') --}}
 
     @if (config('backpack.base.styles') && count(config('backpack.base.styles')))

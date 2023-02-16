6a1e891640e26cba2144129d868e1cbb11f416c3
Pedro X <pxpm88@gmail.com>
2023-02-16 12:45:34 +0000

    fix db raw

--- src/app/Models/Traits/HasEnumFields.php
+++ src/app/Models/Traits/HasEnumFields.php
@@ -20,9 +20,13 @@ trait HasEnumFields
 
         $instance = new static(); // create an instance of the model to be able to get the table name
         $connectionName = $instance->getConnectionName();
-
+        $connection = DB::connection($connectionName);
         try {
-            $type = DB::connection($connectionName)->select(DB::raw('SHOW COLUMNS FROM `'.$table_prefix.$instance->getTable().'` WHERE Field = "'.$field_name.'"'))[0]->Type;
+            $select = app()->version() < 10 ?
+                        DB::raw('SHOW COLUMNS FROM `'.$table_prefix.$instance->getTable().'` WHERE Field = "'.$field_name.'"') :
+                        DB::raw('SHOW COLUMNS FROM `'.$table_prefix.$instance->getTable().'` WHERE Field = "'.$field_name.'"')->getValue($connection->getQueryGrammar());
+
+            $type = $connection->select($select)[0]->Type;
         } catch (\Exception $e) {
             abort(500, 'Enum field type is not supported - it only works on MySQL. Please use select_from_array instead.');
         }

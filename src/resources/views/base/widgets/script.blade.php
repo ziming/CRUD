d1f61039e139e66b85bafee8bcceccfa2c0e058d
Mauro Martinez <maurohmartinez@gmail.com>
2023-02-27 12:05:54 +0200

    Fix Widgets style and script

--- src/resources/views/base/widgets/script.blade.php
+++ src/resources/views/base/widgets/script.blade.php
@@ -1,6 +1,6 @@
 @php
     $src = asset($widget['src'] ?? $widget['content'] ?? $widget['path']);
-    $attributes = collect($widget)->except(['name', 'section', 'type', 'stack', 'src', 'content', 'path'])
+    $attributes = collect($widget)->except(['name', 'section', 'type', 'stack', 'src', 'content', 'path'])->toArray();
 @endphp
 
 @push($widget['stack'] ?? 'after_scripts')

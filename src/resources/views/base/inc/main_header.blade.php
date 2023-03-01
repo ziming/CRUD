745b43ab05b68776b48e9baffe5aecc551978160
Mauro Martinez <maurohmartinez@gmail.com>
2023-03-01 20:23:57 +0200

    Sidebar and header - part 1

--- src/resources/views/base/inc/main_header.blade.php
+++ src/resources/views/base/inc/main_header.blade.php
@@ -1,11 +1,31 @@
-<header class="{{ config('backpack.base.class.header') }}">
-    <div class="container-fluid">
-        <button class="header-toggler px-md-0 me-md-3" type="button" onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()"  aria-label="{{ trans('backpack::base.toggle_navigation')}}">
-            <svg class="icon icon-lg" viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'>
-                <path stroke='#869AB8' stroke-width='2.25' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22' />
-            </svg>
-        </button>
-
-        @include(backpack_view('inc.menu'))
-    </div>
-</header>
+<div class="sticky-top mb-3">
+    <header class="navbar navbar-expand-md navbar-light sticky-top d-print-none">
+        <div class="container-xl">
+            {{--mobile button--}}
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <span></span>
+            <div class="navbar-nav flex-row order-md-last">
+                <div class="d-none d-md-flex me-2">
+                    <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable dark mode" data-bs-original-title="Enable dark mode">
+                        <!-- Download SVG icon from http://tabler-icons.io/i/moon -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+                        </svg>
+                    </a>
+                    <a href="?theme=light" class="nav-link px-0 hide-theme-light" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable light mode" data-bs-original-title="Enable light mode">
+                        <!-- Download SVG icon from http://tabler-icons.io/i/sun -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                            <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
+                        </svg>
+                    </a>
+                </div>
+                @include(backpack_view('inc.menu'))
+            </div>
+        </div>
+    </header>
+</div>

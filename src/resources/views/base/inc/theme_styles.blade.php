318f09732e77b9522163f4ea39526b2e84a88c91
Mauro Martinez <maurohmartinez@gmail.com>
2023-03-01 18:36:33 +0200

    First commit

--- src/resources/views/base/inc/theme_styles.blade.php
+++ src/resources/views/base/inc/theme_styles.blade.php
@@ -1,12 +1,12 @@
 @basset('https://unpkg.com/bootstrap@5.2.3/dist/css/bootstrap.min.css')
-@basset('https://unpkg.com/@coreui/coreui@4.2.4/dist/css/coreui.min.css')
+@basset('https://unpkg.com/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css')
+@basset('https://unpkg.com/@tabler/core@1.0.0-beta17/dist/css/tabler-flags.min.css')
+@basset('https://unpkg.com/@tabler/core@1.0.0-beta17/dist/css/tabler-payments.min.css')
+@basset('https://unpkg.com/@tabler/core@1.0.0-beta17/dist/css/tabler-vendors.min.css')
 @basset('https://unpkg.com/animate.css@4.1.1/animate.compat.css')
 @basset('https://unpkg.com/noty@3.2.0-beta-deprecated/lib/noty.css')
 
 @basset('https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css')
-@basset('https://coreui.io/demos/bootstrap/4.2/free/vendors/simplebar/css/simplebar.css')
-@basset('https://coreui.io/demos/bootstrap/4.2/free/css/vendors/simplebar.css')
-@basset('https://coreui.io/demos/bootstrap/4.2/free/css/style.css')
 
 <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap"
@@ -16,7 +16,5 @@
 true, [], 'style') --}}
 
 {{-- Custom Backpack Rules --}}
-@basset(base_path('vendor/backpack/crud/src/resources/assets/css/coreui4.css'))
+@basset(base_path('vendor/backpack/crud/src/resources/assets/css/tabler.css'))
 @basset(base_path('vendor/backpack/crud/src/resources/assets/css/common.css'))
-
-{{-- TODO: change to Tabler --}}

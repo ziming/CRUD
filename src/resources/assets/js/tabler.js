077833a870310b3ffac2bf417479276347019d54
Mauro Martinez <maurohmartinez@gmail.com>
2023-03-02 16:57:12 +0200

    Fix theming

--- src/resources/assets/js/tabler.js
+++ src/resources/assets/js/tabler.js
@@ -1,18 +1,18 @@
-// Handle theming light|dark
-const mode = window.localStorage.getItem('mode'),
-    body = document.getElementsByTagName('body')[0];
-
 function switchTheme() {
+    const mode = window.localStorage.getItem('mode');
     !mode || mode === 'light'
         ? setTheme('dark')
         : setTheme('light');
 }
 
 function setTheme(theme) {
+    const body = document.getElementsByTagName('body')[0];
     const previousTheme = theme === 'light' ? 'dark' : 'light';
+
     body.classList.remove('theme-' + previousTheme);
     body.classList.add('theme-' + theme);
+
     window.localStorage.setItem('mode', theme);
 }
 
-setTheme(mode);
+setTheme(window.localStorage.getItem('mode'));

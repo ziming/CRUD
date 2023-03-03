06998a8dbb98008dc5d4be4b1c1f7ad7ea0e3a7f
Mauro Martinez <maurohmartinez@gmail.com>
2023-03-03 15:56:43 +0200

    Fix include Bootstrap 5 twice

--- src/resources/views/base/inc/theme_scripts.blade.php
+++ src/resources/views/base/inc/theme_scripts.blade.php
@@ -1,4 +1,4 @@
-@basset('https://unpkg.com/bootstrap@5.2.3/dist/js/bootstrap.min.js')
+{{--@basset('https://unpkg.com/bootstrap@5.2.3/dist/js/bootstrap.min.js')--}}
 @basset('https://unpkg.com/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js')
 @basset('https://unpkg.com/simplebar@latest/dist/simplebar.min.js')
 

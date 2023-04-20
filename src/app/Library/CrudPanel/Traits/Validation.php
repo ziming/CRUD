199e68b7145a5650f331f6ff57571094d60024c9
Pedro X <pxpm88@gmail.com>
2023-04-20 11:29:03 +0100

    add astherics on custom rules

--- src/app/Library/CrudPanel/Traits/Validation.php
+++ src/app/Library/CrudPanel/Traits/Validation.php
@@ -2,6 +2,7 @@
 
 namespace Backpack\CRUD\app\Library\CrudPanel\Traits;
 
+use Backpack\CRUD\app\Library\Uploaders\Validation\ValidBackpackUpload;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Str;
 
@@ -36,77 +37,6 @@ trait Validation
         $messages = $this->getValidationMessagesFromFieldsAndSubfields($fields);
 
         $this->setValidationFromArray($rules, $messages);
-    }
-
-    /**
-     * Return the rules for the fields and subfields in the current crud panel.
-     *
-     * @param  array  $fields
-     * @return array
-     */
-    private function getValidationRulesFromFieldsAndSubfields($fields)
-    {
-        $rules = collect($fields)
-            ->filter(function ($value, $key) {
-                // only keep fields where 'validationRules' OR there are subfields
-                return array_key_exists('validationRules', $value) || array_key_exists('subfields', $value);
-            })->map(function ($item, $key) {
-                $validationRules = [];
-                // only keep the rules, not the entire field definition
-                if (isset($item['validationRules'])) {
-                    $validationRules[$key] = $item['validationRules'];
-                }
-                // add validation rules for subfields
-                if (array_key_exists('subfields', $item)) {
-                    $subfieldsWithValidation = array_filter($item['subfields'], function ($subfield) {
-                        return array_key_exists('validationRules', $subfield);
-                    });
-
-                    foreach ($subfieldsWithValidation as $subfield) {
-                        $validationRules[$item['name'].'.*.'.$subfield['name']] = $subfield['validationRules'];
-                    }
-                }
-
-                return $validationRules;
-            })->toArray();
-
-        return array_merge(...array_values($rules));
-    }
-
-    /**
-     * Return the messages for the fields and subfields in the current crud panel.
-     *
-     * @param  array  $fields
-     * @return array
-     */
-    private function getValidationMessagesFromFieldsAndSubfields($fields)
-    {
-        $messages = [];
-        collect($fields)
-            ->filter(function ($value, $key) {
-                // only keep fields where 'validationMessages' OR there are subfields
-                return array_key_exists('validationMessages', $value) || array_key_exists('subfields', $value);
-            })->each(function ($item, $key) use (&$messages) {
-                if (isset($item['validationMessages'])) {
-                    foreach ($item['validationMessages'] as $rule => $message) {
-                        $messages[$key.'.'.$rule] = $message;
-                    }
-                }
-                // add messages from subfields
-                if (array_key_exists('subfields', $item)) {
-                    $subfieldsWithValidationMessages = array_filter($item['subfields'], function ($subfield) {
-                        return array_key_exists('validationRules', $subfield);
-                    });
-
-                    foreach ($subfieldsWithValidationMessages as $subfield) {
-                        foreach ($subfield['validationMessages'] ?? [] as $rule => $message) {
-                            $messages[$item['name'].'.*.'.$subfield['name'].'.'.$rule] = $message;
-                        }
-                    }
-                }
-            })->toArray();
-
-        return $messages;
     }
 
     /**
@@ -233,6 +163,196 @@ trait Validation
     }
 
     /**
+     * Parse a FormRequest class, figure out what inputs are required
+     * and store this knowledge in the current object.
+     *
+     * @param  string|array  $classOrRulesArray  Class that extends FormRequest or rules array
+     */
+    public function setRequiredFields($classOrRulesArray)
+    {
+        $requiredFields = $this->getOperationSetting('requiredFields') ?? [];
+
+        if (is_array($classOrRulesArray)) {
+            $rules = $classOrRulesArray;
+        } else {
+            $formRequest = new $classOrRulesArray();
+            $rules = $formRequest->rules();
+        }
+
+        if (count($rules)) {
+            foreach ($rules as $key => $validationRules) {
+                if (is_string($validationRules)) {
+                    $validationRules = explode('|', $validationRules);
+                }
+                foreach ($validationRules as $rule) {
+                    if (is_a($rule, ValidBackpackUpload::class, true)) {
+                        foreach ($rule->arrayRules as $arrayRule) {
+                            $key = $this->checkIfRuleIsRequired($key, $arrayRule);
+                            if ($key) {
+                                $requiredFields[] = $key;
+                            }
+                        }
+
+                        continue;
+                    }
+                    $key = $this->checkIfRuleIsRequired($key, $rule);
+                    if ($key) {
+                        $requiredFields[] = $key;
+                    }
+                }
+            }
+        }
+
+        // merge any previous required fields with current ones
+        $requiredFields = array_merge($this->getOperationSetting('requiredFields') ?? [], $requiredFields);
+
+        // since this COULD BE called twice (to support the previous syntax where developers needed to call `setValidation` after the field definition)
+        // and to make this change non-breaking, we are going to return an unique array. There is NO WARM returning repeated names, but there is also
+        // no sense in doing it, so array_unique() it is.
+        $requiredFields = array_unique($requiredFields);
+
+        $this->setOperationSetting('requiredFields', $requiredFields);
+    }
+
+    /**
+     * Check the current object to see if an input is required
+     * for the given operation.
+     *
+     * @param  string  $inputKey  Field or input name.
+     * @param  string  $operation  create / update
+     * @return bool
+     */
+    public function isRequired($inputKey)
+    {
+        if (! $this->hasOperationSetting('requiredFields')) {
+            return false;
+        }
+
+        if (Str::contains($inputKey, '.')) {
+            $inputKey = Str::dotsToSquareBrackets($inputKey, ['*']);
+        }
+
+        return in_array($inputKey, $this->getOperationSetting('requiredFields'));
+    }
+
+    /**
+     * Add the validation setup by developer in field `validationRules` to the crud validation.
+     *
+     * @param  array  $field  - the field we want to get the validation from.
+     * @param  bool|string  $parent  - the parent name when setting up validation for subfields.
+     */
+    private function setupFieldValidation($field, $parent = false)
+    {
+        [$rules, $messages] = $this->getValidationRulesAndMessagesFromField($field, $parent);
+
+        if (! empty($rules)) {
+            $this->setValidation($rules, $messages);
+        }
+    }
+
+    /**
+     * Return the messages for the fields and subfields in the current crud panel.
+     *
+     * @param  array  $fields
+     * @return array
+     */
+    private function getValidationMessagesFromFieldsAndSubfields($fields)
+    {
+        $messages = [];
+        collect($fields)
+            ->filter(function ($value, $key) {
+                // only keep fields where 'validationMessages' OR there are subfields
+                return array_key_exists('validationMessages', $value) || array_key_exists('subfields', $value);
+            })->each(function ($item, $key) use (&$messages) {
+                if (isset($item['validationMessages'])) {
+                    foreach ($item['validationMessages'] as $rule => $message) {
+                        $messages[$key.'.'.$rule] = $message;
+                    }
+                }
+                // add messages from subfields
+                if (array_key_exists('subfields', $item)) {
+                    $subfieldsWithValidationMessages = array_filter($item['subfields'], function ($subfield) {
+                        return array_key_exists('validationRules', $subfield);
+                    });
+
+                    foreach ($subfieldsWithValidationMessages as $subfield) {
+                        foreach ($subfield['validationMessages'] ?? [] as $rule => $message) {
+                            $messages[$item['name'].'.*.'.$subfield['name'].'.'.$rule] = $message;
+                        }
+                    }
+                }
+            })->toArray();
+
+        return $messages;
+    }
+
+    /**
+     * Return the rules for the fields and subfields in the current crud panel.
+     *
+     * @param  array  $fields
+     * @return array
+     */
+    private function getValidationRulesFromFieldsAndSubfields($fields)
+    {
+        $rules = collect($fields)
+            ->filter(function ($value, $key) {
+                // only keep fields where 'validationRules' OR there are subfields
+                return array_key_exists('validationRules', $value) || array_key_exists('subfields', $value);
+            })->map(function ($item, $key) {
+                $validationRules = [];
+                // only keep the rules, not the entire field definition
+                if (isset($item['validationRules'])) {
+                    $validationRules[$key] = $item['validationRules'];
+                }
+                // add validation rules for subfields
+                if (array_key_exists('subfields', $item)) {
+                    $subfieldsWithValidation = array_filter($item['subfields'], function ($subfield) {
+                        return array_key_exists('validationRules', $subfield);
+                    });
+
+                    foreach ($subfieldsWithValidation as $subfield) {
+                        $validationRules[$item['name'].'.*.'.$subfield['name']] = $subfield['validationRules'];
+                    }
+                }
+
+                return $validationRules;
+            })->toArray();
+
+        return array_merge(...array_values($rules));
+    }
+
+    /**
+     * Return the array of rules and messages with the validation key accordingly set
+     * to match the field or the subfield accordingly.
+     *
+     * @param  array  $field  - the field we want to get the rules and messages from.
+     * @param  bool|string  $parent  - the parent name when setting up validation for subfields.
+     */
+    private function getValidationRulesAndMessagesFromField($field, $parent = false)
+    {
+        $rules = [];
+        $messages = [];
+
+        foreach ((array) $field['name'] as $fieldName) {
+            if ($parent) {
+                $fieldName = $parent.'.*.'.$fieldName;
+            }
+
+            if (isset($field['validationRules'])) {
+                $rules[$fieldName] = $field['validationRules'];
+            }
+            if (isset($field['validationMessages'])) {
+                foreach ($field['validationMessages'] as $validator => $message) {
+                    $fieldValidationName = $fieldName.'.'.$validator;
+                    $messages[$fieldValidationName] = $message;
+                }
+            }
+        }
+
+        return [$rules, $messages];
+    }
+
+    /**
      * Return an array containing the request rules and the field/controller rules merged.
      * The rules in request will take precedence over the ones in controller/fields.
      *
@@ -289,112 +409,25 @@ trait Validation
     }
 
     /**
-     * Parse a FormRequest class, figure out what inputs are required
-     * and store this knowledge in the current object.
+     * Check if the given rule is a required rule.
      *
-     * @param  string|array  $classOrRulesArray  Class that extends FormRequest or rules array
+     * @param  string  $key
+     * @param  string  $rule
+     * @return string|bool
      */
-    public function setRequiredFields($classOrRulesArray)
+    private function checkIfRuleIsRequired($key, $rule)
     {
-        $requiredFields = $this->getOperationSetting('requiredFields') ?? [];
-
-        if (is_array($classOrRulesArray)) {
-            $rules = $classOrRulesArray;
-        } else {
-            $formRequest = new $classOrRulesArray();
-            $rules = $formRequest->rules();
-        }
-
-        if (count($rules)) {
-            foreach ($rules as $key => $rule) {
-                if (
-                    (is_string($rule) && strpos($rule, 'required') !== false && strpos($rule, 'required_') === false) ||
-                    (is_array($rule) && array_search('required', $rule) !== false && array_search('required_', $rule) === false)
-                ) {
-                    if (Str::contains($key, '.')) {
-                        $key = Str::dotsToSquareBrackets($key, ['*']);
-                    }
-
-                    $requiredFields[] = $key;
-                }
-            }
-        }
-
-        // merge any previous required fields with current ones
-        $requiredFields = array_merge($this->getOperationSetting('requiredFields') ?? [], $requiredFields);
-
-        // since this COULD BE called twice (to support the previous syntax where developers needed to call `setValidation` after the field definition)
-        // and to make this change non-breaking, we are going to return an unique array. There is NO WARM returning repeated names, but there is also
-        // no sense in doing it, so array_unique() it is.
-        $requiredFields = array_unique($requiredFields);
-
-        $this->setOperationSetting('requiredFields', $requiredFields);
-    }
-
-    /**
-     * Check the current object to see if an input is required
-     * for the given operation.
-     *
-     * @param  string  $inputKey  Field or input name.
-     * @param  string  $operation  create / update
-     * @return bool
-     */
-    public function isRequired($inputKey)
-    {
-        if (! $this->hasOperationSetting('requiredFields')) {
-            return false;
-        }
-
-        if (Str::contains($inputKey, '.')) {
-            $inputKey = Str::dotsToSquareBrackets($inputKey, ['*']);
-        }
-
-        return in_array($inputKey, $this->getOperationSetting('requiredFields'));
-    }
-
-    /**
-     * Add the validation setup by developer in field `validationRules` to the crud validation.
-     *
-     * @param  array  $field  - the field we want to get the validation from.
-     * @param  bool|string  $parent  - the parent name when setting up validation for subfields.
-     */
-    private function setupFieldValidation($field, $parent = false)
-    {
-        [$rules, $messages] = $this->getValidationRulesAndMessagesFromField($field, $parent);
-
-        if (! empty($rules)) {
-            $this->setValidation($rules, $messages);
-        }
-    }
-
-    /**
-     * Return the array of rules and messages with the validation key accordingly set
-     * to match the field or the subfield accordingly.
-     *
-     * @param  array  $field  - the field we want to get the rules and messages from.
-     * @param  bool|string  $parent  - the parent name when setting up validation for subfields.
-     */
-    private function getValidationRulesAndMessagesFromField($field, $parent = false)
-    {
-        $rules = [];
-        $messages = [];
-
-        foreach ((array) $field['name'] as $fieldName) {
-            if ($parent) {
-                $fieldName = $parent.'.*.'.$fieldName;
+        if (
+            (is_string($rule) && strpos($rule, 'required') !== false && strpos($rule, 'required_') === false) ||
+            (is_array($rule) && array_search('required', $rule) !== false && array_search('required_', $rule) === false)
+        ) {
+            if (Str::contains($key, '.')) {
+                $key = Str::dotsToSquareBrackets($key, ['*']);
             }
 
-            if (isset($field['validationRules'])) {
-                $rules[$fieldName] = $field['validationRules'];
-            }
-            if (isset($field['validationMessages'])) {
-                foreach ($field['validationMessages'] as $validator => $message) {
-                    $fieldValidationName = $fieldName.'.'.$validator;
-                    $messages[$fieldValidationName] = $message;
-                }
-            }
+            return $key;
         }
 
-        return [$rules, $messages];
+        return false;
     }
 }

ad8866754887efbeec23f7902cd0d97e9d5d0d7b
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-06-14 14:09:24 +0300

    add quick button blade file

---
+++ src/resources/views/crud/buttons/quick.blade.php
@@ -0,0 +1,22 @@
+@php
+    $access = $parameters['access'] ?? Str::of($button->name)->studly();
+    $icon = $parameters['icon'] ?? '';
+    $label = $parameters['label'] ?? Str::of($button->name)->headline();
+
+    $wrapper = $parameters['wrapper'] ?? [];
+    $wrapper['element'] = $wrapper['element'] ?? 'a';
+    $wrapper['href'] = $wrapper['href'] ?? url($crud->route. ($entry?->getKey() ? '/'.$entry?->getKey().'/' : '/') . Str::of($button->name)->kebab());
+    $wrapper['class'] = $wrapper['class'] ?? ($button->stack == 'top' ? 'btn btn-outline-primary' : 'btn btn-sm btn-link');
+@endphp
+
+@if ($parameters['access'] == true || $crud->hasAccess($access))
+    <{{ $wrapper['element'] }}
+        @foreach ($wrapper as $attribute => $value)
+            @if (is_string($attribute))
+            {{ $attribute }}="{{ $value }}"
+            @endif
+        @endforeach
+        >
+        <i class="{{ $icon }}"></i> {{ $label }}
+    </{{ $wrapper['element'] }}>
+@endif

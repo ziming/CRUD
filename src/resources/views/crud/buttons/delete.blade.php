f99be56bef2f863b8239aa4fdfc4cbfbf37465c7
Antonio Almeida <promatik@gmail.com>
2023-07-10 09:46:18 +0100

    Added span wrapper to all icons

--- src/resources/views/crud/buttons/delete.blade.php
+++ src/resources/views/crud/buttons/delete.blade.php
@@ -1,5 +1,7 @@
 @if ($crud->hasAccess('delete'))
-	<a href="javascript:void(0)" onclick="deleteEntry(this)" data-route="{{ url($crud->route.'/'.$entry->getKey()) }}" class="btn btn-sm btn-link" data-button-type="delete"><i class="la la-trash"></i> {{ trans('backpack::crud.delete') }}</a>
+    <a href="javascript:void(0)" onclick="deleteEntry(this)" data-route="{{ url($crud->route.'/'.$entry->getKey()) }}" class="btn btn-sm btn-link" data-button-type="delete">
+        <span><i class="la la-trash"></i> {{ trans('backpack::crud.delete') }}</span>
+    </a>
 @endif
 
 {{-- Button Javascript --}}
--- src/resources/views/crud/create.blade.php
+++ src/resources/views/crud/create.blade.php
@@ -17,7 +17,11 @@
         <p class="ms-2 ml-2 mb-0" bp-section="page-subheading">{!! $crud->getSubheading() ?? trans('backpack::crud.add').' '.$crud->entity_name !!}.</p>
         @if ($crud->hasAccess('list'))
             <p class="mb-0 ms-2 ml-2" bp-section="page-subheading-back-button">
-                <small><a href="{{ url($crud->route) }}" class="d-print-none font-sm"><i class="la la-angle-double-{{ config('backpack.base.html_direction') == 'rtl' ? 'right' : 'left' }}"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a></small>
+                <small>
+                    <a href="{{ url($crud->route) }}" class="d-print-none font-sm">
+                        <span><i class="la la-angle-double-{{ config('backpack.base.html_direction') == 'rtl' ? 'right' : 'left' }}"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></span>
+                    </a>
+                </small>
             </p>
         @endif
     </section>

20560bd986868bb11baf6e3d704246d6cd91fb7c
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-10-03 12:00:04 +0300

    Access - add Model type, remove useless docblocks if repeated in return types

--- src/app/Library/CrudPanel/Traits/Access.php
+++ src/app/Library/CrudPanel/Traits/Access.php
@@ -3,14 +3,12 @@
 namespace Backpack\CRUD\app\Library\CrudPanel\Traits;
 
 use Backpack\CRUD\app\Exceptions\AccessDeniedException;
+use Illuminate\Database\Eloquent\Model;
 
 trait Access
 {
     /**
      * Set an operation as having access using the Settings API.
-     *
-     * @param  string|array  $operation
-     * @return bool
      */
     public function allowAccess(array|string $operation): bool
     {
@@ -23,11 +21,8 @@ trait Access
 
     /**
      * Disable the access to a certain operation, or the current one.
-     *
-     * @param  string|array  $operation  [description]
-     * @return [type] [description]
      */
-    public function denyAccess(array|string $operation)
+    public function denyAccess(array|string $operation) : bool
     {
         foreach ((array) $operation as $op) {
             $this->set($op.'.access', false);
@@ -38,10 +33,6 @@ trait Access
 
     /**
      * Check if a operation is allowed for a Crud Panel. Return false if not.
-     *
-     * @param  string  $operation
-     * @param  \Model  $entry
-     * @return bool
      */
     public function hasAccess(string $operation, $entry = null): bool
     {
@@ -58,12 +49,8 @@ trait Access
 
     /**
      * Check if any operations are allowed for a Crud Panel. Return false if not.
-     *
-     * @param  string|array  $operation_array
-     * @param  \Model  $entry
-     * @return bool
      */
-    public function hasAccessToAny(array|string $operation_array, $entry = null): bool
+    public function hasAccessToAny(array|string $operation_array, ?Model $entry = null) : bool
     {
         foreach ((array) $operation_array as $key => $operation) {
             if ($this->hasAccess($operation, $entry) == true) {
@@ -76,12 +63,8 @@ trait Access
 
     /**
      * Check if all operations are allowed for a Crud Panel. Return false if not.
-     *
-     * @param  array  $operation_array  Permissions.
-     * @param  \Model  $entry
-     * @return bool
      */
-    public function hasAccessToAll(array|string $operation_array, $entry = null): bool
+    public function hasAccessToAll(array|string $operation_array, ?Model $entry = null) : bool
     {
         foreach ((array) $operation_array as $key => $operation) {
             if (! $this->hasAccess($operation, $entry)) {
@@ -95,13 +78,9 @@ trait Access
     /**
      * Check if a operation is allowed for a Crud Panel. Fail if not.
      *
-     * @param  string  $operation
-     * @param  \Model  $entry
-     * @return bool
-     *
      * @throws \Backpack\CRUD\Exception\AccessDeniedException in case the operation is not enabled
      */
-    public function hasAccessOrFail(string $operation, $entry = null): bool
+    public function hasAccessOrFail(string $operation, ?Model $entry = null) : bool
     {
         if (! $this->hasAccess($operation, $entry)) {
             throw new AccessDeniedException(trans('backpack::crud.unauthorized_access', ['access' => $operation]));
@@ -113,9 +92,6 @@ trait Access
     /**
      * Get an operation's access condition, if set. A condition
      * can be anything, but usually a boolean or a callable.
-     *
-     * @param  string  $operation
-     * @return bool|callable|null
      */
     public function getAccessCondition(string $operation): bool|callable|null
     {
@@ -124,10 +100,6 @@ trait Access
 
     /**
      * Set the condition under which an operation is allowed for a Crud Panel.
-     *
-     * @param  string|array  $operation
-     * @param  bool|callable|null  $condition
-     * @return void
      */
     public function setAccessCondition(array|string $operation, bool|callable|null $condition): void
     {
@@ -139,9 +111,6 @@ trait Access
     /**
      * Check if an operation has an access condition already set.
      * A condition can be anything, but usually a boolean or a callable.
-     *
-     * @param  string  $operation
-     * @return bool
      */
     public function hasAccessCondition(string $operation): bool
     {

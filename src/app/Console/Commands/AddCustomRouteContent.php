07f3d225466aedaa86f66d9c43ed0a87b33003cd
pxpm <pxpm88@gmail.com>
2024-07-26 18:56:24 +0100

    fix {} use case

--- src/app/Console/Commands/AddCustomRouteContent.php
+++ src/app/Console/Commands/AddCustomRouteContent.php
@@ -85,8 +85,31 @@ class AddCustomRouteContent extends Command
             return;
         }
 
+       
+
+        // in case the last line contains the last } but also the last {, we need to split them
+        // so that we can create a space between them and add the new code
+        if (strpos($cleanContent[$lastLine], '{') !== false) {
+            $lastLineContent = explode('{', $originalContent[$lastLine]);
+            $originalContent[$lastLine] = $lastLineContent[0].'{'.PHP_EOL;
+            // push all other elements one line down creating space for the new code
+            for ($i = count($originalContent) - 1; $i > $lastLine; $i--) {
+                $originalContent[$i+1] = $originalContent[$i];
+            }
+            $originalContent[$lastLine+1] = $lastLineContent[1];
+            $lastLine++;
+        }
+
+        $sliceLength = 0;
+
+         // in case there is already an empty line at the end of the route file, we don't need to add another one
+         if(trim($originalContent[$lastLine - 1]) === '') {
+            $lastLine--;
+            $sliceLength = 1;
+        }
+
         // add the code to the line before the last line
-        array_splice($originalContent, $lastLine, 0, '    '.$code.PHP_EOL);
+        array_splice($originalContent, $lastLine, $sliceLength, '    '.$code.PHP_EOL);
 
         // write the new content to the file
         if (file_put_contents($routeFilePath, implode('', $originalContent)) === false) {

e6947d304d7f812de752962524750f32a5fde1eb
pxpm <pxpm88@gmail.com>
2024-12-19 12:20:53 +0000

    fix php 8.4 dynamic relationships

--- src/app/Library/CrudPanel/Traits/Relationships.php
+++ src/app/Library/CrudPanel/Traits/Relationships.php
@@ -274,7 +274,9 @@ trait Relationships
     {
         $relation = $this->getRelationInstance($field);
 
-        if (Str::afterLast($field['name'], '.') === $relation->getRelationName() || Str::endsWith($relation->getRelationName(), '{closure}')) {
+        if (Str::afterLast($field['name'], '.') === $relation->getRelationName() ||
+            Str::endsWith($relation->getRelationName(), '{closure}') || // php < 8.3
+            Str::startsWith($relation->getRelationName(), '{closure:')) { // php 8.4+
             return $relation->getForeignKeyName();
         }
 
